611d3c10d3237c4620fc37c8ef9276ba5e45c8e3
Steven R. Brandt <sbrandt@cct.lsu.edu>
2017-10-02 10:14:27 -0500

    Update with master branch and add copyrights.

--- py/Phylanx2.py
+++ py/Phylanx2.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017 Hartmut Kaiser
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 import phylanx.ast as ast
 import phylanx.util as util
 # A simple way to turn a python3 expression into a data structure (AST)
