ec62475a8da426cbb0418f3b1b45afc27a45f51e
Bibek Wagle <justwagle@gmail.com>
2017-10-03 13:39:52 -0500

    WIP: Adding exponential primitive

--- phylanx/execution_tree/primitives/exponential_operation.hpp
+++ phylanx/execution_tree/primitives/exponential_operation.hpp
@@ -6,4 +6,37 @@
 #ifndef PHYLANX_EXPONENTIAL_OPERATION_HPP_OCT031241PM
 #define PHYLANX_EXPONENTIAL_OPERATION_HPP_OCT031241PM
 
+#include <phylanx/config.hpp>
+#include <phylanx/ast/node.hpp>
+#include <phylanx/ir/node_data.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+
+#include <hpx/include/components.hpp>
+
+#include <utility>
+
+namespace phylanx { namespace execution_tree { namespace primitives
+    {
+      class HPX_COMPONENT_EXPORT exponential_operation
+        : public base_primitive
+        , public hpx::components::component_base<exponential_operation>
+      {
+      private:
+        using operands_type = std::vector<ir::node_data<double>>;
+        primitive_argument_type operands_;
+
+      public:
+        exponential_operation() = default;
+
+        exponential_operation(primitive_argument_type &&operands);
+
+        hpx::future<ir::node_data<double>> eval() const override;
+
+      protected:
+        ir::node_data<double> exp0d(operands_type const& ops) const;
+        ir::node_data<double> expxd(operands_type const& ops) const;
+      };
+
+    }}}
+
 #endif //PHYLANX_EXPONENTIAL_OPERATION_HPP_OCT031241PM
--- src/execution_tree/primitives/exponential_operation.cpp
+++ src/execution_tree/primitives/exponential_operation.cpp
@@ -2,3 +2,19 @@
 //
 //   Distributed under the Boost Software License, Version 1.0. (See accompanying
 //   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/execution_tree/primitives/exponential_operation.hpp>
+
+#include <hpx/include/components.hpp>
+
+////////////////////////////////////////////////////////////////////////////////////////
+typedef hpx::components::component<
+    phylanx::execution_tree::primitives::exponential_operation>
+    exponential_operation_type;
+HPX_REGISTER_DERIVED_COMPONENT_FACTORY(exponential_operation_type
+    , phylanx_exponential_operation_type
+    , "phylanx_primitive_component"
+    , hpx::components::factory_enabled)
+HPX_DEFINE_GET_COMPONENT_TYPE(exponential_operation_type::wrapped_type)
+///////////////////////////////////////////////////////////////////////////////////////
+
