4df9b0f9c0860f5d2f66c57ea9e93a887d452976
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-10-15 19:50:31 -0500

    Fixing test

--- tests/unit/execution_tree/primitives/define_operation.cpp
+++ tests/unit/execution_tree/primitives/define_operation.cpp
@@ -8,6 +8,26 @@
 #include <hpx/hpx_main.hpp>
 #include <hpx/util/lightweight_test.hpp>
 
+void test_define_operation_var(char const* expr, char const* name,
+    std::vector<char const*> const& args, char const* body)
+{
+    phylanx::execution_tree::variables variables;
+    phylanx::execution_tree::functions functions;
+
+    auto result = phylanx::execution_tree::detail::generate_tree(
+        phylanx::ast::generate_ast(expr),
+        phylanx::execution_tree::detail::generate_patterns(
+            phylanx::execution_tree::get_all_known_patterns()),
+        variables, functions);
+
+    HPX_TEST_EQ(variables.size(), std::size_t(1));
+    HPX_TEST_EQ(functions.size(), std::size_t(0));
+
+    // verify function name
+    auto p = functions.find(name);
+    HPX_TEST(p.first == p.second);
+}
+
 void test_define_operation(char const* expr, char const* name,
     std::vector<char const*> const& args, char const* body)
 {
@@ -44,7 +64,7 @@ void test_define_operation(char const* expr, char const* name,
 
 int main(int argc, char* argv[])
 {
-    test_define_operation("define(x, 3.14)", "x", {}, "3.14");
+    test_define_operation_var("define(x, 3.14)", "x", {}, "3.14");
     test_define_operation("define(y, x, x + 1)", "y", {"x"}, "x + 1");
     test_define_operation("define(add, x, y, x + y)", "add", {"x", "y"}, "x + y");
 
