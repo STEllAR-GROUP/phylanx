ed54017c1827eac861ff37a6532984d9b17d4e7a
Parsa Amini <me@parsaamini.net>
2017-10-20 01:02:49 -0500

    Vcpkg support for Blaze added. Ordering changed.

--- cmake/Phylanx_SetupBlaze.cmake
+++ cmake/Phylanx_SetupBlaze.cmake
@@ -8,31 +8,39 @@
 # TODO: We don't use `phylanx_setup_eigen3`. We can remove `Phylanx_SetupEigen3.cmake`.
 
 macro(phylanx_setup_blaze)
-  # Method 1: find_package
-  # NOTE: `blaze_DIR` being set assists this step.
-  find_package(blaze NO_CMAKE_PACKAGE_REGISTRY)
-  if(blaze_FOUND)
-    add_library(blaze_target INTERFACE)
-    target_link_libraries(blaze_target INTERFACE blaze::blaze)
+  # Method 2: Specify the include directory `blaze_INCLUDE_DIR`
+  if(blaze_INCLUDE_DIR)
+    message("Adding defined blaze_INCLUDE_DIR to header include directories.")
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${blaze_INCLUDE_DIR})
+    include_directories(${blaze_INCLUDE_DIR})
+  endif()
+  # Method 3: Vcpkg
+  if(_VCPKG_ROOT_DIR)
+    message("Vcpkg found. Adding to header include directory: " ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+    list(APPEND CMAKE_REQUIRED_INCLUDES "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include")
+    include_directories("${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include")
+  endif()
+
+  # Check if Math.h is available
+  include(CheckIncludeFileCXX)
+  if(MSCVC)
+    set(CMAKE_REQUIRED_FLAGS "/std:c++14")
   else()
-    # Method 2: Specify the include directory `blaze_INCLUDE_DIR`
-    if(blaze_INCLUDE_DIR)
-      list(APPEND CMAKE_REQUIRED_INCLUDES ${blaze_INCLUDE_DIR})
-      include_directories(${blaze_INCLUDE_DIR})
-    endif()
-    # Check if Math.h is available
-    include(CheckIncludeFileCXX)
-    if(MSCVC)
-      set(CMAKE_REQUIRED_FLAGS "/std:c++14")
+    set(CMAKE_REQUIRED_FLAGS "-std=c++14")
+  endif()
+  check_include_file_cxx(blaze/Math.h HAVE_BLAZE_MATH_H)
+  if(NOT HAVE_BLAZE_MATH_H)
+    # Method 1: find_package
+    # NOTE: `blaze_DIR` being set assists this step.
+    find_package(blaze NO_CMAKE_PACKAGE_REGISTRY)
+    if(blaze_FOUND)
+      add_library(blaze_target INTERFACE)
+      target_link_libraries(blaze_target INTERFACE blaze::blaze)
     else()
-      set(CMAKE_REQUIRED_FLAGS "-std=c++14")
-    endif()
-    check_include_file_cxx(blaze/Math.h HAVE_BLAZE_MATH_H)
-    if(NOT HAVE_BLAZE_MATH_H)
-       # HACK: PHYLANX_SKIP_BLAZE_CHECK
-       # - If Blaze is available through vcpkg, vcpkg's toolchain script does
-       #   not offer any variables to detect blaze's location when this script
-       #   is running.
+      # HACK: PHYLANX_SKIP_BLAZE_CHECK
+      # - If Blaze is available through vcpkg, vcpkg's toolchain script does
+      #   not offer any variables to detect blaze's location when this script
+      #   is running.
       if(NOT PHYLANX_SKIP_BLAZE_CHECK)
         phylanx_error("Blaze could not be found. Please set blaze_DIR to help locating it.")
       else()
@@ -40,9 +48,6 @@ macro(phylanx_setup_blaze)
       endif()
     endif()
   endif()
-
-  
-
 
   phylanx_info("Blaze was found.")
 
