189644cf978fc51c5504a14bb9a11ea44e1f4d3f
aserio <aserio@cct.lsu.edu>
2017-10-20 16:31:03 -0500

    Renaming structs  - loop -> if_impl

--- src/execution_tree/primitives/if_conditional.cpp
+++ src/execution_tree/primitives/if_conditional.cpp
@@ -32,15 +32,13 @@ HPX_DEFINE_GET_COMPONENT_TYPE(if_conditional_type::wrapped_type)
 namespace phylanx {
 namespace execution_tree {
     namespace primitives {
-    ///////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////
         std::vector<match_pattern_type> const if_conditional::match_data = {
             hpx::util::make_tuple(
-                "if", "if(_1, _2, _3)", &create<if_conditional>)
-          , hpx::util::make_tuple(
-                "if", "if(_1, _2)", &create<if_conditional>)
-        };
+                "if", "if(_1, _2, _3)", &create<if_conditional>),
+            hpx::util::make_tuple("if", "if(_1, _2)", &create<if_conditional>)};
 
-    ///////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////
         if_conditional::if_conditional(
             std::vector<primitive_argument_type>&& operands)
           : operands_(std::move(operands))
@@ -71,48 +69,50 @@ namespace execution_tree {
             }
         }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // struct containing implementation of evaluate 
-    // (used to resolve lifetime issues)
-    namespace detail
-    {
-        struct loop : std::enable_shared_from_this<loop>
-        {
-            loop(std::vector<primitive_argument_type> const& operands)
-              : operands_(operands)
-            {}
-
-            hpx::future<primitive_result_type> body()
+        ///////////////////////////////////////////////////////////////////////////
+        // struct containing implementation of evaluate
+        // (used to resolve lifetime issues)
+        namespace detail {
+            struct if_impl : std::enable_shared_from_this<if_impl>
             {
-                // Keep data alive with a shared pointer
-                auto this_ = this -> shared_from_this();
-                hpx::future<std::uint8_t> cond_eval = boolean_operand(operands_[0]);
-                return cond_eval.then(
-                    [this_](hpx::future<std::uint8_t>&& cond_eval) {
-                        if (cond_eval.get() != 0)
-                        {
-                            return literal_operand(this_->operands_[1]);
-                        }
-                        else if (this_->operands_.size() > 2)
-                        {
-                            return literal_operand(this_->operands_[2]);
-                        }
-                        else
-                        {
-                            return hpx::make_ready_future(primitive_result_type{});
-                        }
-                    });
-            }
+                if_impl(std::vector<primitive_argument_type> const& operands)
+                  : operands_(operands)
+                {
+                }
 
-        private:
-            std::vector<primitive_argument_type> operands_;
-        };
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    // evaluate 'true_case' or 'false_case' based on 'cond'
+                hpx::future<primitive_result_type> body()
+                {
+                    // Keep data alive with a shared pointer
+                    auto this_ = this->shared_from_this();
+                    hpx::future<std::uint8_t> cond_eval =
+                        boolean_operand(operands_[0]);
+                    return cond_eval.then(
+                        [this_](hpx::future<std::uint8_t>&& cond_eval) {
+                            if (cond_eval.get() != 0)
+                            {
+                                return literal_operand(this_->operands_[1]);
+                            }
+                            else if (this_->operands_.size() > 2)
+                            {
+                                return literal_operand(this_->operands_[2]);
+                            }
+                            else
+                            {
+                                return hpx::make_ready_future(
+                                    primitive_result_type{});
+                            }
+                        });
+                }
+
+            private:
+                std::vector<primitive_argument_type> operands_;
+            };
+        }
+        ///////////////////////////////////////////////////////////////////////////
+        // evaluate 'true_case' or 'false_case' based on 'cond'
         hpx::future<primitive_result_type> if_conditional::eval() const
         {
-            return std::make_shared<detail::loop>(operands_)->body();
+            return std::make_shared<detail::if_impl>(operands_)->body();
         }
     }
 }
