8c9f410dac56803f6e6ce2a5c84ea45b5272a6aa
Bibek Wagle <justwagle@gmail.com>
2017-10-24 11:56:43 -0500

    WIP: Adding skeleton for slicing operation

---
+++ phylanx/execution_tree/primitives/slicing_operation.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2017 Bibek Wagle
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)#ifndef PHYLANX_SLICING_OPERATION_HPP
+#define PHYLANX_SLICING_OPERATION_1153_10242017_HPP
+
+#endif //PHYLANX_SLICING_OPERATION_1153_10242017_HPP
