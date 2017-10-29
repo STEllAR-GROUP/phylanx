d8cb3463d780ea6d25bdf4308d7a10cd21bf55c7
Parsa Amini <me@parsaamini.net>
2017-10-29 11:26:00 -0500

    renamed variables and functions in inverse_operation.cpp unit test

--- tests/unit/execution_tree/primitives/inverse_operation.cpp
+++ tests/unit/execution_tree/primitives/inverse_operation.cpp
@@ -13,30 +13,30 @@
 #include <vector>
 #include <utility>
 
-void test_transpose_operation_0d()
+void test_inversion_0d()
 {
     phylanx::execution_tree::primitive lhs =
         hpx::new_<phylanx::execution_tree::primitives::variable>(
             hpx::find_here(), phylanx::ir::node_data<double>(5.0));
 
-    phylanx::execution_tree::primitive transpose =
+    phylanx::execution_tree::primitive inversion =
         hpx::new_<phylanx::execution_tree::primitives::inverse_operation>(
             hpx::find_here(),
             std::vector<phylanx::execution_tree::primitive_argument_type>{
                 std::move(lhs)});
 
     hpx::future<phylanx::execution_tree::primitive_result_type> f =
-        transpose.eval();
+        inversion.eval();
 
     HPX_TEST_EQ(
         0.2, phylanx::execution_tree::extract_numeric_value(f.get())[0]);
 }
 
-void test_transpose_operation_0d_lit()
+void test_inversion_0d_lit()
 {
     phylanx::ir::node_data<double> lhs(5.0);
 
-    phylanx::execution_tree::primitive transpose =
+    phylanx::execution_tree::primitive inversion =
         hpx::new_<phylanx::execution_tree::primitives::inverse_operation>(
             hpx::find_here(),
             std::vector<phylanx::execution_tree::primitive_argument_type>{
@@ -44,13 +44,13 @@ void test_transpose_operation_0d_lit()
             });
 
     hpx::future<phylanx::execution_tree::primitive_result_type> f =
-        transpose.eval();
+        inversion.eval();
 
     HPX_TEST_EQ(
         0.2, phylanx::execution_tree::extract_numeric_value(f.get())[0]);
 }
 
-void test_transpose_operation_2d()
+void test_inversion_2d()
 {
     blaze::Rand<blaze::DynamicMatrix<double>> gen{};
     blaze::DynamicMatrix<double> m = gen.generate(42UL, 42UL);
@@ -59,7 +59,7 @@ void test_transpose_operation_2d()
         hpx::new_<phylanx::execution_tree::primitives::variable>(
             hpx::find_here(), phylanx::ir::node_data<double>(m));
 
-    phylanx::execution_tree::primitive transpose =
+    phylanx::execution_tree::primitive inversion =
         hpx::new_<phylanx::execution_tree::primitives::inverse_operation>(
             hpx::find_here(),
             std::vector<phylanx::execution_tree::primitive_argument_type>{
@@ -67,7 +67,7 @@ void test_transpose_operation_2d()
             });
 
     hpx::future<phylanx::execution_tree::primitive_result_type> f =
-        transpose.eval();
+        inversion.eval();
 
     blaze::DynamicMatrix<double> expected = blaze::inv(m);
     HPX_TEST_EQ(
@@ -77,10 +77,10 @@ void test_transpose_operation_2d()
 
 int main(int argc, char* argv[])
 {
-    test_transpose_operation_0d();
-    test_transpose_operation_0d_lit();
+    test_inversion_0d();
+    test_inversion_0d_lit();
     
-    test_transpose_operation_2d();
+    test_inversion_2d();
 
     return hpx::util::report_errors();
 }
