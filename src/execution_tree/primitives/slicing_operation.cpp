81c7c69aa03a1273c7db1e4fe49a56de55e411d1
Bibek Wagle <justwagle@gmail.com>
2017-10-29 18:40:01 -0500

    Adding Slicing Primitive

--- src/execution_tree/primitives/slicing_operation.cpp
+++ src/execution_tree/primitives/slicing_operation.cpp
@@ -31,123 +31,147 @@ HPX_DEFINE_GET_COMPONENT_TYPE(slicing_operation_type::wrapped_type)
 
 ///////////////////////////////////////////////////////////////////////////////
 namespace phylanx { namespace execution_tree { namespace primitives
-    {
-        ///////////////////////////////////////////////////////////////////////////
-        std::vector<match_pattern_type> const slicing_operation::match_data =
-                {
-                        hpx::util::make_tuple(
-                                "slice", "slice(_1, _2, _3, _4, _5)",
-                                &create<slicing_operation>)
-                };
-
-        ///////////////////////////////////////////////////////////////////////////
-        slicing_operation::slicing_operation(
-                std::vector<primitive_argument_type> && operands)
-                : operands_(std::move(operands))
-        {}
-
-        ///////////////////////////////////////////////////////////////////////////
-        namespace detail
-        {
-            struct slicing : std::enable_shared_from_this<slicing>
+{
+    ///////////////////////////////////////////////////////////////////////////
+    std::vector<match_pattern_type> const slicing_operation::match_data =
             {
-                slicing() = default;
+                    hpx::util::make_tuple(
+                            "slice", "slice(_1, _2, _3, _4, _5)",
+                            &create<slicing_operation>)
+            };
 
-            protected:
-                using arg_type = ir::node_data<double>;
-                using args_type = std::vector<arg_type>;
+    ///////////////////////////////////////////////////////////////////////////
+    slicing_operation::slicing_operation(
+            std::vector<primitive_argument_type> && operands)
+            : operands_(std::move(operands))
+    {}
 
-                primitive_result_type slicing0d(args_type && args) const
+    ///////////////////////////////////////////////////////////////////////////
+    namespace detail
+    {
+        struct slicing : std::enable_shared_from_this<slicing>
+        {
+            slicing() = default;
+
+        protected:
+            using arg_type = ir::node_data<double>;
+            using args_type = std::vector<arg_type>;
+
+            using matrix_type = blaze::DynamicMatrix<double>;
+            using submatrix_type = blaze::Submatrix<matrix_type> ;
+
+            primitive_result_type slicing0d(args_type && args) const
+            {
+                //return the input as it is if the input is of zero dimensions
+                //the values passed to row_start, row_stop, col_start, col_stop
+                //does not have an effect on the result.
+
+                return primitive_result_type(std::move(args[0]));
+            }
+
+            primitive_result_type slicing1d(args_type && args) const
+            {
+                //return elements starting from col_start to col_stop
+                //the values passed to row_stat and row_stop does not have an
+                //effect on the result.
+
+                auto col_start  = extract_integer_value(args[3]);
+                auto col_stop = extract_integer_value(args[4]);
+
+                submatrix_type sm = blaze::submatrix(args[0].matrix()
+                        ,0,col_start,1,col_stop);
+
+                matrix_type result = sm;
+                return primitive_result_type(std::move(result));
+            }
+
+            primitive_result_type slicing2d(args_type && args) const
+            {
+                //returns the sliced matrix, depending uopn the values
+                //provided in row_start, row_stop, col_start, col_stop
+                
+                auto row_start = extract_integer_value(args[1]);
+                auto row_stop = extract_integer_value(args[2]);
+                auto col_start  = extract_integer_value(args[3]);
+                auto col_stop = extract_integer_value(args[4]);
+
+                submatrix_type sm = blaze::submatrix(args[0].matrix()
+                        ,row_start,col_start,row_stop,col_stop);
+                matrix_type result = sm;
+                return primitive_result_type(std::move(result));
+            }
+
+
+        public:
+            hpx::future<primitive_result_type> eval(
+                    std::vector<primitive_argument_type> const& operands,
+                    std::vector<primitive_argument_type> const& args) const
+            {
+                if (operands.size() != 5)
                 {
-                    //return the input as it is if the input is of zero dimensions
-                    return primitive_result_type(args[0]);
+                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                                        "phylanx::execution_tree::primitives"
+                                        "::slicing_operation::slicing_operation",
+                                        "the slicing_operation primitive requires "
+                                        "exactly five arguments");
                 }
 
-                primitive_result_type slicing1d(args_type && args) const
+                bool arguments_valid = true;
+                for (std::size_t i = 0; i != operands.size(); ++i)
                 {
-                    HPX_THROW_EXCEPTION(hpx::not_implemented,"slice1d",
-                                        "slice1d not implemented yet");
-
-                }
-
-                primitive_result_type slicing2d(args_type && args) const
-                {
-                    HPX_THROW_EXCEPTION(hpx::not_implemented,"slice2d",
-                                        "slice2d not implemented yet");
-                }
-
-
-            public:
-                hpx::future<primitive_result_type> eval(
-                        std::vector<primitive_argument_type> const& operands,
-                        std::vector<primitive_argument_type> const& args) const
-                {
-                    if (operands.size() != 5)
-                    {
-                        HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                                            "phylanx::execution_tree::primitives"
-                                            "::slicing_operation::slicing_operation",
-                                            "the slicing_operation primitive requires "
-                                            "exactly five arguments");
-                    }
-
-                  bool arguments_valid = true;
-                  for (std::size_t i = 0; i != operands.size(); ++i)
-                  {
                     if (!valid(operands[i]))
                     {
-                      arguments_valid = false;
+                        arguments_valid = false;
                     }
-                  }
+                }
 
-                  if (!arguments_valid)
-                  {
+                if (!arguments_valid)
+                {
                     HPX_THROW_EXCEPTION(hpx::bad_parameter,
                                         "slicing_operation::eval", "the "
-                                        "slicing_operation primitive requires that "
-                                        "the arguments given by the operands array "
-                                        "are valid");
-                  }
+                                         "slicing_operation primitive requires that "
+                                         "the arguments given by the operands array "
+                                         "are valid");
+                }
 
-                  auto this_ = this->shared_from_this();
-                  return hpx::dataflow(hpx::util::unwrapping(
-                          [this_](args_type && args) -> primitive_result_type
-                          {
-                              std::size_t lhs_dims = args[0].num_dimensions();
-                              switch (lhs_dims)
-                              {
+                auto this_ = this->shared_from_this();
+                return hpx::dataflow(hpx::util::unwrapping(
+                        [this_](args_type && args) -> primitive_result_type
+                        {
+                            std::size_t lhs_dims = args[0].num_dimensions();
+                            switch (lhs_dims)
+                            {
                                 case 0:
-                                  return this_->slicing0d(std::move(args));
+                                    return this_->slicing0d(std::move(args));
 
                                 case 1:
-                                  return this_->slicing1d(std::move(args));
+                                    return this_->slicing1d(std::move(args));
 
                                 case 2:
-                                  return this_->slicing2d(std::move(args));
+                                    return this_->slicing2d(std::move(args));
 
                                 default:
-                                  HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                                                      "slicing_operation::eval",
-                                                      "left hand side operand "
-                                                      "has unsupported number "
-                                                      "of dimensions");
-                              }
-                          }),
-                             detail::map_operands(operands, numeric_operand, args)
-                  );
-                }
-            };
-        }
+                                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                                                        "slicing_operation::eval",
+                                                        "left hand side operand "
+                                                        "has unsupported number "
+                                                        "of dimensions");
+                            }
+                        }), detail::map_operands(operands, numeric_operand, args)
+                );
+            }
+        };
+    }
 
-        hpx::future<primitive_result_type> slicing_operation::eval(
-                std::vector<primitive_argument_type> const& args) const
+    hpx::future<primitive_result_type> slicing_operation::eval(
+            std::vector<primitive_argument_type> const& args) const
+    {
+        if (operands_.empty())
         {
-          if (operands_.empty())
-          {
             static std::vector<primitive_argument_type> noargs;
             return std::make_shared<detail::slicing>()->eval(args, noargs);
-          }
-
-          return std::make_shared<detail::slicing>()->eval(operands_, args);
         }
+
+        return std::make_shared<detail::slicing>()->eval(operands_, args);
+    }
 }}}
--- tests/unit/execution_tree/primitives/slicing_operation.cpp
+++ tests/unit/execution_tree/primitives/slicing_operation.cpp
@@ -16,27 +16,27 @@
 
 void test_slicing_operation_0d()
 {
-  phylanx::execution_tree::primitive first =
+    phylanx::execution_tree::primitive first =
       hpx::new_<phylanx::execution_tree::primitives::variable>(
           hpx::find_here(), phylanx::ir::node_data<double>(42.0));
 
-  phylanx::execution_tree::primitive second =
+    phylanx::execution_tree::primitive second =
       hpx::new_<phylanx::execution_tree::primitives::variable>(
           hpx::find_here(), phylanx::ir::node_data<double>(5.0));
 
-  phylanx::execution_tree::primitive third =
+    phylanx::execution_tree::primitive third =
       hpx::new_<phylanx::execution_tree::primitives::variable>(
           hpx::find_here(), phylanx::ir::node_data<double>(47.0));
 
-  phylanx::execution_tree::primitive fourth =
+    phylanx::execution_tree::primitive fourth =
       hpx::new_<phylanx::execution_tree::primitives::variable>(
           hpx::find_here(), phylanx::ir::node_data<double>(5.0));
 
-  phylanx::execution_tree::primitive fifth =
+    phylanx::execution_tree::primitive fifth =
       hpx::new_<phylanx::execution_tree::primitives::variable>(
           hpx::find_here(), phylanx::ir::node_data<double>(15.0));
 
-  phylanx::execution_tree::primitive slice =
+    phylanx::execution_tree::primitive slice =
       hpx::new_<phylanx::execution_tree::primitives::slicing_operation>(
           hpx::find_here(),
           std::vector<phylanx::execution_tree::primitive_argument_type>{
@@ -44,15 +44,105 @@ void test_slicing_operation_0d()
               std::move(fourth),std::move(fifth)
           });
 
-  hpx::future<phylanx::execution_tree::primitive_result_type> f =
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
       slice.eval();
-  
-  HPX_TEST_EQ(42.0, phylanx::execution_tree::extract_numeric_value(f.get())[0]);
+
+    HPX_TEST_EQ(42.0, phylanx::execution_tree::extract_numeric_value(f.get())[0]);
+}
+
+void test_slicing_operation_1d()
+{
+    blaze::Rand<blaze::DynamicVector<double, blaze::rowVector>> gen{};
+    blaze::DynamicVector<double, blaze::rowVector> v1 = gen.generate(1007UL);
+
+    phylanx::execution_tree::primitive input_vector =
+            hpx::new_<phylanx::execution_tree::primitives::variable>(
+                    hpx::find_here(), phylanx::ir::node_data<double>(v1));
+
+    phylanx::execution_tree::primitive row_start =
+            hpx::new_<phylanx::execution_tree::primitives::variable>(
+                    hpx::find_here(), phylanx::ir::node_data<double>(0));
+
+    phylanx::execution_tree::primitive row_stop =
+            hpx::new_<phylanx::execution_tree::primitives::variable>(
+                    hpx::find_here(), phylanx::ir::node_data<double>(0));
+
+    phylanx::execution_tree::primitive col_start =
+            hpx::new_<phylanx::execution_tree::primitives::variable>(
+                    hpx::find_here(), phylanx::ir::node_data<double>(5.0));
+
+    phylanx::execution_tree::primitive col_stop =
+            hpx::new_<phylanx::execution_tree::primitives::variable>(
+                    hpx::find_here(), phylanx::ir::node_data<double>(15.0));
+
+    phylanx::execution_tree::primitive slice =
+            hpx::new_<phylanx::execution_tree::primitives::slicing_operation>(
+                    hpx::find_here(),
+                    std::vector<phylanx::execution_tree::primitive_argument_type>{
+                            std::move(input_vector), std::move(row_start)
+                            , std::move(row_stop), std::move(col_start)
+                            , std::move(col_stop)
+                    });
+
+    auto sm = blaze::subvector(v1,5,15);
+    auto expected = sm;
+
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
+            slice.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(std::move(expected)),
+                phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+void test_slicing_operation_2d()
+{
+    blaze::Rand<blaze::DynamicMatrix<double>> gen{};
+    blaze::DynamicMatrix<double> m1 = gen.generate(101UL, 101UL);
+
+    phylanx::execution_tree::primitive input_matrix =
+          hpx::new_<phylanx::execution_tree::primitives::variable>(
+                  hpx::find_here(), phylanx::ir::node_data<double>(m1));
+
+    phylanx::execution_tree::primitive row_start =
+          hpx::new_<phylanx::execution_tree::primitives::variable>(
+                  hpx::find_here(), phylanx::ir::node_data<double>(5.0));
+
+    phylanx::execution_tree::primitive row_stop =
+          hpx::new_<phylanx::execution_tree::primitives::variable>(
+                  hpx::find_here(), phylanx::ir::node_data<double>(47.0));
+
+    phylanx::execution_tree::primitive col_start =
+          hpx::new_<phylanx::execution_tree::primitives::variable>(
+                  hpx::find_here(), phylanx::ir::node_data<double>(5.0));
+
+    phylanx::execution_tree::primitive col_stop =
+          hpx::new_<phylanx::execution_tree::primitives::variable>(
+                  hpx::find_here(), phylanx::ir::node_data<double>(15.0));
+
+    phylanx::execution_tree::primitive slice =
+          hpx::new_<phylanx::execution_tree::primitives::slicing_operation>(
+                  hpx::find_here(),
+                  std::vector<phylanx::execution_tree::primitive_argument_type>{
+                          std::move(input_matrix), std::move(row_start)
+                          , std::move(row_stop), std::move(col_start)
+                          , std::move(col_stop)
+                  });
+
+    auto sm = blaze::submatrix(m1,5,5,47,15);
+    auto expected = sm;
+
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
+          slice.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(std::move(expected)),
+              phylanx::execution_tree::extract_numeric_value(f.get()));
 }
 
 int main(int argc, char* argv[])
 {
   test_slicing_operation_0d();
+  test_slicing_operation_1d();
+  test_slicing_operation_2d();
 
   return hpx::util::report_errors();
 }
