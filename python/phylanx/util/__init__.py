596f1df88e75e3e163bfa64b2aa4a46cc83a895c
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-01-11 16:10:24 -0600

    Commits a version of lra that uses more of numpy.

--- python/phylanx/util/__init__.py
+++ python/phylanx/util/__init__.py
@@ -228,8 +228,9 @@ class Recompiler:
           raise Exception(nm)
 
 def convert_to_phylanx_type(v):
+    import numpy
     t = type(v)
-    if t == int or t == float or t == list:
+    if t == int or t == float or t == list or t == numpy.ndarray:
         return et.var(v)
     else:
         return v
