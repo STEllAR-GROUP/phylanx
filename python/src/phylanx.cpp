1dab8e717d1acbbaff15b6a0e709c8fe1e129ea6
Steven R. Brandt <sbrandt@cct.lsu.edu>
2018-01-11 11:26:32 -0600

    Add copyrights.

--- python/src/phylanx.cpp
+++ python/src/phylanx.cpp
@@ -1,5 +1,6 @@
 //  Copyright (c) 2017 Hartmut Kaiser
 //  Copyright (c) 2018 R. Tohid
+//  Copyright (c) 2018 Steven R. Brandt
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
