5a405258081e8c5de9f50187a92cbd6b7accb2ff
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-01-16 18:48:16 -0600

    Adding compose_primitive_name() (opposite of parse_primitive_name())

--- phylanx/execution_tree/compiler/primitive_name.hpp
+++ phylanx/execution_tree/compiler/primitive_name.hpp
@@ -3,8 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(PHYLANX_EXECUTION_TREE_PARSE_PRIMITIVE_NAME_HPP)
-#define PHYLANX_EXECUTION_TREE_PARSE_PRIMITIVE_NAME_HPP
+#if !defined(PHYLANX_EXECUTION_TREE_PRIMITIVE_NAME_HPP)
+#define PHYLANX_EXECUTION_TREE_PRIMITIVE_NAME_HPP
 
 #include <phylanx/config.hpp>
 
@@ -47,8 +47,13 @@ namespace phylanx { namespace execution_tree { namespace compiler
         std::int64_t tag;
     };
 
+    // Split the given primitive name into its parts
     PHYLANX_EXPORT primitive_name_parts parse_primitive_name(
         std::string const& name);
+
+    // compose a new primitive name from the given parts
+    PHYLANX_EXPORT std::string compose_primitive_name(
+        primitive_name_parts const& parts);
 }}}
 
 #endif
