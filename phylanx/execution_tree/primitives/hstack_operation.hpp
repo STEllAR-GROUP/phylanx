50593875aaf24db0157f9b6c964f9153d082052a
Bibek Wagle <justwagle@gmail.com>
2018-01-26 01:08:55 -0600

    Adding hstack and vstack primitives

---
+++ phylanx/execution_tree/primitives/hstack_operation.hpp
@@ -0,0 +1,39 @@
+//  Copyright (c) 2018 Bibek Wagle
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef PHYLANX_HSTACK_OPERATION_JAN13_1200_2018_H
+#define PHYLANX_HSTACK_OPERATION_JAN13_1200_2018_H
+
+#include <phylanx/config.hpp>
+#include <phylanx/ast/node.hpp>
+#include <phylanx/ir/node_data.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+#include <phylanx/util/optional.hpp>
+#include <phylanx/util/serialization/optional.hpp>
+
+#include <hpx/include/components.hpp>
+
+#include <vector>
+
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+    class HPX_COMPONENT_EXPORT hstack_operation
+      : public base_primitive
+      , public hpx::components::component_base<hstack_operation>
+    {
+    public:
+        static std::vector<match_pattern_type> const match_data;
+
+        hstack_operation() = default;
+
+        hstack_operation(std::vector<primitive_argument_type>&& operands);
+
+        hpx::future<primitive_result_type> eval(
+            std::vector<primitive_argument_type> const& params) const override;
+    };
+}}}
+
+
+#endif //PHYLANX_HSTACK_OPERATION_JAN13_1200_2018_H
---
+++ tests/unit/execution_tree/primitives/vstack_operation.cpp
@@ -0,0 +1,113 @@
+//  Copyright (c) 2018 Bibek Wagle
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/phylanx.hpp>
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/lcos.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+void vstack_operation_0d()
+{
+    phylanx::execution_tree::primitive first =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(42.0));
+
+    phylanx::execution_tree::primitive second =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(5.0));
+
+    phylanx::execution_tree::primitive vstack =
+        hpx::new_<phylanx::execution_tree::primitives::vstack_operation>(
+            hpx::find_here(),
+            std::vector<phylanx::execution_tree::primitive_argument_type>{
+                std::move(first), std::move(second)});
+
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
+        vstack.eval();
+
+    blaze::DynamicMatrix<double> expected{{42.0},
+                                          {5.0}};
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(std::move(expected)),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+void vstack_operation_1d()
+{
+    blaze::DynamicVector<double> v1{1, 2, 3, 4, 5};
+    blaze::DynamicVector<double> v2{11, 12, 13, 14, 15};
+
+    phylanx::execution_tree::primitive first =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(v1));
+
+    phylanx::execution_tree::primitive second =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(v2));
+
+    phylanx::execution_tree::primitive vstack =
+        hpx::new_<phylanx::execution_tree::primitives::vstack_operation>(
+            hpx::find_here(),
+            std::vector<phylanx::execution_tree::primitive_argument_type>{
+                std::move(first), std::move(second)});
+
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
+        vstack.eval();
+
+    blaze::DynamicMatrix<double> expected{
+        {1, 2, 3, 4, 5},
+        {11, 12, 13, 14, 15}};
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(std::move(expected)),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+void vstack_operation_2d()
+{
+    blaze::DynamicMatrix<double> m1{{1, 2, 3},
+                                    {4, 5, 6}};
+    blaze::DynamicMatrix<double> m2{{11, 22, 33},
+                                    {12, 13, 33}};
+
+    phylanx::execution_tree::primitive first =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(m1));
+
+    phylanx::execution_tree::primitive second =
+        hpx::new_<phylanx::execution_tree::primitives::variable>(
+            hpx::find_here(), phylanx::ir::node_data<double>(m2));
+
+    phylanx::execution_tree::primitive vstack =
+        hpx::new_<phylanx::execution_tree::primitives::vstack_operation>(
+            hpx::find_here(),
+            std::vector<phylanx::execution_tree::primitive_argument_type>{
+                std::move(first), std::move(second)});
+
+    blaze::DynamicMatrix<double> expected{
+        {1, 2, 3},
+        {4, 5, 6},
+        {11, 22, 33},
+        {12, 13, 33}};
+
+    hpx::future<phylanx::execution_tree::primitive_result_type> f =
+        vstack.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(std::move(expected)),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+int main(int argc, char* argv[])
+{
+    vstack_operation_0d();
+    vstack_operation_1d();
+    vstack_operation_2d();
+
+    return hpx::util::report_errors();
+}
