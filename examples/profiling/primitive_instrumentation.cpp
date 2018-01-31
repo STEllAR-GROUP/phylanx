e7d7b1e5c000c0090ebddd0434788641d2849536
Parsa Amini <parsa@cct.lsu.edu>
2018-01-31 15:22:17 -0600

    Primitive instrumentation example added

--- examples/profiling/primitive_instrumentation.cpp
+++ examples/profiling/primitive_instrumentation.cpp
@@ -10,79 +10,31 @@
 #include <hpx/include/agas.hpp>
 #include <hpx/runtime_fwd.hpp>
 
-#include <cstddef>
-#include <cstdint>
 #include <iostream>
-#include <map>
-#include <vector>
 #include <string>
 #include <utility>
 
-#include <boost/program_options.hpp>
-#include <blaze/Math.h>
-
-//////////////////////////////////////////////////////////////////////////////////
-// This example uses part of the breast cancer dataset from UCI Machine Learning
-// Repository:
-//     https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic)
-//
-// A copy of the full dataset in CSV format (breast_cancer.csv), obtained from
-// scikit-learn datasets, is provided in the same folder as this example.
-//
-// The layout of the data in the provided CSV file used by the example
-// is as follows:
-// 30 features per line followed by the classification
-// 569 lines of data
-//
-// This example also demonstrates how the generated primitives can be introspected
-// and linked back to the source code.
-//
-/////////////////////////////////////////////////////////////////////////////////
-
-std::string const read_x_code = R"(block(
-    //
-    // Read X-data from given CSV file
-    //
-    define(read_x, filepath, row_start, row_stop, col_start, col_stop,
-        slice(file_read_csv(filepath), row_start, row_stop, col_start, col_stop)
-    ),
-    read_x
-))";
-
-std::string const read_y_code = R"(block(
-    //
-    // Read Y-data from given CSV file
-    //
-    define(read_y, filepath, row_start, row_stop,
-        slice(file_read_csv(filepath), row_start, row_stop, -1, 0)
-    ),
-    read_y
-))";
-
 ///////////////////////////////////////////////////////////////////////////////
-std::string const lra_code = R"(block(
+char const* const lra_code = R"(block(
     //
     // Logistic regression analysis algorithm
     //
-    //   x: [N, M]
-    //   y: [N]
-    //
-    define(lra, x, y, alpha, iterations, enable_output,
+    //   x: [30, 2]
+    //   y: [30]
+    define(lra, x, y, alpha,
         block(
-            define(weights, constant(0.0, shape(x, 1))),            // weights: [M]
-            define(transx, transpose(x)),                           // transx:  [M, N]
+            define(weights, constant(0.0, shape(x, 1))),     // weights: [2]
+            define(transx, transpose(x)),                    // transx:  [2, 30]
             define(pred, constant(0.0, shape(x, 0))),
             define(error, constant(0.0, shape(x, 0))),
             define(gradient, constant(0.0, shape(x, 1))),
             define(step, 0),
             while(
-                step < iterations,
+                step < 10,
                 block(
-                    if(enable_output, cout("step: ", step, ", ", weights)),
-                    // exp(-dot(x, weights)): [N], pred: [N]
                     store(pred, 1.0 / (1.0 + exp(-dot(x, weights)))),
-                    store(error, pred - y),                         // error: [N]
-                    store(gradient, dot(transx, error)),            // gradient: [M]
+                    store(error, pred - y),                  // error: [30]
+                    store(gradient, dot(transx, error)),     // gradient: [2]
                     parallel_block(
                         store(weights, weights - (alpha * gradient)),
                         store(step, step + 1)
@@ -96,205 +48,132 @@ std::string const lra_code = R"(block(
 ))";
 
 ///////////////////////////////////////////////////////////////////////////////
-// Find the line/column position in the source code from a given iterator
-// pointing into it.
-//
-std::pair<std::size_t, std::size_t> get_pos(std::string const& code,
-    std::int64_t pos)
+int hpx_main()
 {
-    std::size_t line = 1;
-    std::size_t column = 1;
-
-    for (std::int64_t i = 0; i != pos && i != code.size(); ++i)
-    {
-        if (code[i] == '\r' || code[i] == '\n')    // CR/LF
-        {
-            ++line;
-            column = 1;
-        }
-        else
-        {
-            ++column;
-        }
-    }
-
-    return std::make_pair(line, column);
-}
-
-// Extract the compile_id/tag pair from a given primitive instance name.
-//
-// The compile_id is a sequence number tracking invocations of the
-// function phylanx::execution_tree::compile (needed to link back to the
-// concrete source code compiled).
-//
-// The tag is an index into the array of iterators filled by
-// phylanx::ast::generate_ast. It allows to find the iterator referring
-// to the construct in the source code a particular primitive instance was
-// created by.
-//
-std::pair<std::size_t, std::int64_t> extract_tags(std::string const& name)
-{
-    auto data = phylanx::execution_tree::compiler::parse_primitive_name(name);
-    return std::make_pair(data.compile_id, data.tag);
-}
-
-// The symbolic names registered in AGAS that identify the created
-// primitive instances have the following structure:
-//
-//      /phylanx/<primitive>#<sequence-nr>[#<instance>]/<compile_id>#<tag>
-//
-//  where:
-//      <primitive>:   the name of primitive type representing the given
-//                     node in the expression tree
-//      <sequence-nr>: the sequence number of the corresponding instance
-//                     of type <primitive>
-//      <instance>:    (optional), some primitives have additional instance
-//                     names, for instance references to function arguments
-//                     have the name of the argument as their <instance>
-//      <compile_id>:  the sequence number of the invocation of the
-//                     function phylanx::execution_tree::compile
-//      <tag>:         the position inside the compiled code block where the
-//                     referring to the point of usage of the primitive in
-//                     the compiled source code
-//
-void print_instrumentation(
-    char const* const name, int compile_id, std::string const& code,
-    phylanx::execution_tree::compiler::function const& func,
-    std::map<std::string, hpx::id_type> const& entries)
-{
-    std::cout << "Instrumentation information for function: " << name << "\n";
-
-    for (auto const& e : entries)
-    {
-        // extract compile_id and iterator index (tag) from the symbolic name
-        auto tags = extract_tags(e.first);
-        if (tags.first != compile_id)
-            continue;
-
-        // find real position of given symbol in source code
-        if (tags.second >= 0)
-        {
-            auto pos = get_pos(code, tags.second);
-            std::cout << e.first << ": " << name << "(" << pos.first << ", "
-                      << pos.second << "): ";
-
-            // show the next (at max) 20 characters
-            auto end = code.begin() + tags.second;
-            for (int i = 0; end != code.end() && i != 20; ++end, ++i)
-            {
-                if (*end == '\n' || *end == '\r')
-                    break;
-            }
-            std::cout << std::string(code.begin() + tags.second, end) << " ...\n";
-        }
-        else
-        {
-            std::cout << e.first << "\n";
-        }
-    }
-
-    std::cout << "\n";
-
-    std::cout << "Tree information for function: " << name << "\n";
-    std::cout << phylanx::execution_tree::newick_tree(
-                     name, func.get_expression_topology())
-              << "\n\n";
-
-    std::cout << phylanx::execution_tree::dot_tree(
-                     name, func.get_expression_topology())
-              << "\n\n";
-}
-
-///////////////////////////////////////////////////////////////////////////////
-int hpx_main(boost::program_options::variables_map& vm)
-{
-    if (vm.count("data_csv") == 0)
-    {
-        std::cerr << "Please specify '--data_csv=data-file'";
-        return hpx::finalize();
-    }
-
     // compile the given code
     phylanx::execution_tree::compiler::function_list snippets;
 
-    auto read_x = phylanx::execution_tree::compile_and_run(
-        phylanx::ast::generate_ast(read_x_code), snippets);
-    auto read_y = phylanx::execution_tree::compile_and_run(
-        phylanx::ast::generate_ast(read_y_code), snippets);
     auto lra = phylanx::execution_tree::compile_and_run(
         phylanx::ast::generate_ast(lra_code), snippets);
 
     // print instrumentation information, if enabled
-    if (vm.count("instrument") != 0)
-    {
-        auto entries =
-            hpx::agas::find_symbols(hpx::launch::sync, "/phylanx/*");
+    auto entries =
+        hpx::agas::find_symbols(hpx::launch::sync, "/phylanx/*");
 
-        print_instrumentation("read_x", 0, read_x_code, read_x, entries);
-        print_instrumentation("read_y", 1, read_y_code, read_y, entries);
-        print_instrumentation("lra", 2, lra_code, lra, entries);
-    }
+    // LRA arguments
 
-    auto row_start = vm["row_start"].as<std::int64_t>();
-    auto col_start = vm["col_start"].as<std::int64_t>();
-    auto row_stop = vm["row_stop"].as<std::int64_t>();
-    auto col_stop = vm["col_stop"].as<std::int64_t>();
+    blaze::DynamicMatrix<double> v1{ { 15.04, 16.74 },{ 13.82, 24.49 },
+    { 12.54, 16.32 },{ 23.09, 19.83 },{ 9.268, 12.87 },{ 9.676, 13.14 },
+    { 12.22, 20.04 },{ 11.06, 17.12 },{ 16.3 , 15.7 },{ 15.46, 23.95 },
+    { 11.74, 14.69 },{ 14.81, 14.7 },{ 13.4 , 20.52 },{ 14.58, 13.66 },
+    { 15.05, 19.07 },{ 11.34, 18.61 },{ 18.31, 20.58 },{ 19.89, 20.26 },
+    { 12.88, 18.22 },{ 12.75, 16.7 },{ 9.295, 13.9 },{ 24.63, 21.6 },
+    { 11.26, 19.83 },{ 13.71, 18.68 },{ 9.847, 15.68 },{ 8.571, 13.1 },
+    { 13.46, 18.75 },{ 12.34, 12.27 },{ 13.94, 13.17 },{ 12.07, 13.44 } };
 
-    // read the data from the files
-    auto x = read_x(vm["data_csv"].as<std::string>(), row_start, row_stop,
-        col_start, col_stop);
-    auto y = read_y(vm["data_csv"].as<std::string>(), row_start, row_stop);
+    blaze::DynamicVector<double> v2{ 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0,
+        1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+    auto x = phylanx::ir::node_data<double>{ v1 };
+    auto y = phylanx::ir::node_data<double>{ v2 };
+    auto alpha = phylanx::ir::node_data<double>{ 1e-5 };
+    bool enable_output = true;
 
-    // remaining command line options
-    auto alpha = vm["alpha"].as<double>();
-    auto iterations = vm["num_iterations"].as<std::int64_t>();
-    bool enable_output = vm.count("enable_output") != 0;
-
-    // time execution
+    // Time execution
     hpx::util::high_resolution_timer t;
 
-    // evaluate LRA using the read data
-    auto result =
-        lra(std::move(x), std::move(y), alpha, iterations, enable_output);
+    // Evaluate LRA using the read data
+    auto result = lra(x, y, alpha);
+
+    // CSV Header
+    std::cout << "primitive" << ","
+        << "sequence_number" << ","
+        << "instance_id" << ","
+        << "compile_id" << ","
+        << "tag1" << ","
+        << "tag2" << ","
+        << "count" << ","
+        << "time" << ","
+        << "direct_count" << ","
+        << "direct_time"
+        << std::endl;
+
+    // Complement performance data from primitive performance counters with
+    // information in their AGAS names
+    for (auto const& pattern :
+        phylanx::execution_tree::get_all_known_patterns())
+    {
+        std::string const& name = hpx::util::get<0>(pattern);
+
+        std::string count_pc_name(
+            "/phylanx{locality#0/total}/primitives/" + name + "/count/eval");
+        hpx::performance_counters::performance_counter count_pc(
+            count_pc_name);
+        auto count_pc_values =
+            count_pc.get_counter_values_array(hpx::launch::sync, false);
+
+        std::string time_pc_name(
+            "/phylanx{locality#0/total}/primitives/" + name + "/time/eval");
+        hpx::performance_counters::performance_counter time_pc(time_pc_name);
+        auto time_pc_values =
+            time_pc.get_counter_values_array(hpx::launch::sync, false);
+
+        std::string direct_count_pc_name(
+            "/phylanx{locality#0/total}/primitives/" + name +
+            "/count/eval_direct");
+        hpx::performance_counters::performance_counter direct_count_pc(
+            direct_count_pc_name);
+        auto direct_count_pc_values =
+            direct_count_pc.get_counter_values_array(hpx::launch::sync, false);
+
+        std::string direct_time_pc_name(
+            "/phylanx{locality#0/total}/primitives/" + name +
+            "/time/eval_direct");
+        hpx::performance_counters::performance_counter direct_time_pc(
+            direct_time_pc_name);
+        auto direct_time_pc_values =
+            direct_time_pc.get_counter_values_array(hpx::launch::sync, false);
+
+        // Query AGAS for primitives
+        auto entries = hpx::agas::find_symbols(
+            hpx::launch::sync, "/phylanx/" + name + "#*");
+
+        for (auto const& e : entries)
+        {
+            // Parse the primitive name
+            auto tags = phylanx::execution_tree::compiler::parse_primitive_name(
+                e.first);
+
+            // HACK: block 0 does not appear in AGAS
+            if (name == "block" &&
+                (tags.sequence_number == 0 ||
+                    tags.sequence_number == count_pc_values.values_.size()))
+            {
+                continue;
+            }
+
+            std::cout << "\"" << name << "\"" << ","
+                << tags.sequence_number << ","
+                << tags.instance << ","
+                << tags.compile_id << ","
+                << tags.tag1 << ","
+                << tags.tag2 << ","
+                << count_pc_values.values_[tags.sequence_number] << ","
+                << time_pc_values.values_[tags.sequence_number] << ","
+                << direct_count_pc_values.values_[tags.sequence_number] << ","
+                << direct_time_pc_values.values_[tags.sequence_number]
+                << std::endl;
+        }
+    }
 
     auto elapsed = t.elapsed();
 
-    std::cout << "Result: \n"
-              << phylanx::execution_tree::extract_numeric_value(result) << "\n"
-              << "Calculated in: " << elapsed << " seconds\n";
+    // Make sure all counters are properly initialized, don't reset current counter values
+    hpx::reinit_active_counters(false);
 
     return hpx::finalize();
 }
 
 int main(int argc, char* argv[])
 {
-    // command line handling
-    boost::program_options::options_description desc("usage: lra [options]");
-    desc.add_options()
-        ("enable_output,e", "enable progress output (default: false)")
-        ("instrument,i", "print instrumentation information (default: false)")
-        ("num_iterations,n",
-            boost::program_options::value<std::int64_t>()->default_value(750),
-            "number of iterations (default: 750)")
-        ("alpha,a",
-            boost::program_options::value<double>()->default_value(1e-5),
-            "alpha (default: 1e-5)")
-        ("data_csv",
-            boost::program_options::value<std::string>(),
-            "file name for reading data")
-        ("row_start",
-            boost::program_options::value<std::int64_t>()->default_value(0),
-            "row_start (default: 0)")
-        ("col_start",
-            boost::program_options::value<std::int64_t>()->default_value(0),
-            "col_start (default: 0)")
-        ("row_stop",
-            boost::program_options::value<std::int64_t>()->default_value(569),
-            "row_stop (default: 569)")
-        ("col_stop",
-            boost::program_options::value<std::int64_t>()->default_value(30),
-            "col_stop (default: 30)")
-        ;
-
-    return hpx::init(desc, argc, argv);
+    return hpx::init(argc, argv);
 }
