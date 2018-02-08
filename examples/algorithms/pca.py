0d65414e9c69e743d0f8e8f4f60052f500cea5df
ct-clmsn <ct-clmsn(at)gmail>
2018-02-07 23:03:44 -0500

    initial import of pca

---
+++ examples/algorithms/pca.py
@@ -0,0 +1,37 @@
+#  Copyright (c) 2018 Steven R. Brandt
+#  Copyright (c) 2018 Christopher Taylor
+#
+#  Distributed under the Boost Software License, Version 1.0. (See accompanying
+#  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+#  Implementation as presented here:
+#
+#  https://glowingpython.blogspot.com/2011/07/principal-component-analysis-with-numpy.html
+#  
+#  Additional implementation:
+#
+#  https://glowingpython.blogspot.com/2011/07/pca-and-image-compression-with-numpy.html
+#
+#  Special thanks to John Tukey for providing explanations of this algorithm
+#  and an implementation available in the public domain
+#
+import phylanx
+from phylanx.util import *
+
+@phyfun
+def pca(A):
+    M = transpose(A - mean( transpose(A), axis=1))
+    latent, coeff = eig(cov(M))
+    score = dot(transpose(coeff), M)
+    return coeff, score, latent
+
+if __name__ == "__main__":
+
+    A = array([ [2.4,0.7,2.9,2.2,3.0,2.7,1.6,1.1,1.6,0.9],
+                [2.5,0.5,2.2,1.9,3.1,2.3,2,1,1.5,1.1] ])
+
+    coeff, score, latent = princomp(transpose(A))
+    phy_print(coeff, score, latent)
+
+    res = princomp(transpose(A))
+    phy_print(res)
