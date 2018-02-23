c80b5a2e73ffaf602961500a35a3048761a5151c
Parsa Amini <parsa@cct.lsu.edu>
2018-02-23 11:38:48 -0600

    Move some examples misplaced in unit test directory

--- examples/algorithms/als.py
+++ examples/algorithms/als.py
@@ -1,7 +1,7 @@
-#   Copyright (c) 2017 Shahrzad Shirzad
+# Copyright (c) 2017 Shahrzad Shirzad
 #
-#   Distributed under the Boost Software License, Version 1.0. (See accompanying
-#   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 import numpy as np
 
