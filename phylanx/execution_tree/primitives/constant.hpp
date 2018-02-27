005abb062e8d2f663d975c1235677af2a12de73c
Parsa Amini <parsa@cct.lsu.edu>
2018-02-27 16:34:08 -0600

    Refactor const

--- phylanx/execution_tree/primitives/constant.hpp
+++ phylanx/execution_tree/primitives/constant.hpp
@@ -1,7 +1,7 @@
-//   Copyright (c) 2017-2018 Hartmut Kaiser
+// Copyright (c) 2017-2018 Hartmut Kaiser
 //
-//   Distributed under the Boost Software License, Version 1.0. (See accompanying
-//   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(PHYLANX_PRIMITIVES_CONSTANT_OCT_10_2017_0243PM)
 #define PHYLANX_PRIMITIVES_CONSTANT_OCT_10_2017_0243PM
@@ -9,16 +9,28 @@
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/base_primitive.hpp>
 #include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/ir/node_data.hpp>
 
 #include <hpx/lcos/future.hpp>
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace phylanx { namespace execution_tree { namespace primitives
 {
-    class constant : public primitive_component_base
+    class constant
+        : public primitive_component_base
+        , public std::enable_shared_from_this<constant>
     {
+    protected:
+        hpx::future<primitive_argument_type> eval(
+            std::vector<primitive_argument_type> const& operands,
+            std::vector<primitive_argument_type> const& args) const;
+
+        using operand_type = ir::node_data<double>;
+        using operands_type = std::vector<operand_type>;
+
     public:
         static match_pattern_type const match_data;
 
@@ -29,6 +41,14 @@ namespace phylanx { namespace execution_tree { namespace primitives
 
         hpx::future<primitive_argument_type> eval(
             std::vector<primitive_argument_type> const& args) const override;
+
+    private:
+        primitive_argument_type constant0d(operand_type && op) const;
+        primitive_argument_type constant1d(
+            operand_type&& op, std::size_t dim) const;
+        primitive_argument_type constant2d(operand_type&& op,
+            operand_type::dimensions_type const& dim) const;
+
     };
 
     PHYLANX_EXPORT primitive create_constant(hpx::id_type const& locality,
