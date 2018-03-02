bc03cc78447218069c8b140bea5842c48cbd63b3
Parsa Amini <parsa@cct.lsu.edu>
2018-03-02 08:27:00 -0600

    Refactored inverse_operation primitive

--- phylanx/execution_tree/primitives/inverse_operation.hpp
+++ phylanx/execution_tree/primitives/inverse_operation.hpp
@@ -1,7 +1,7 @@
-//   Copyright (c) 2017-2018 Hartmut Kaiser
+// Copyright (c) 2017-2018 Hartmut Kaiser
 //
-//   Distributed under the Boost Software License, Version 1.0. (See accompanying
-//   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(PHYLANX_PRIMITIVES_INVERSE_OPERATION_OCT_09_2017_0154PM)
 #define PHYLANX_PRIMITIVES_INVERSE_OPERATION_OCT_09_2017_0154PM
@@ -9,16 +9,28 @@
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/base_primitive.hpp>
 #include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/ir/node_data.hpp>
 
 #include <hpx/lcos/future.hpp>
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace phylanx { namespace execution_tree { namespace primitives
 {
-    class inverse_operation : public primitive_component_base
+    class inverse_operation
+        : public primitive_component_base
+        , std::enable_shared_from_this<inverse_operation>
     {
+    protected:
+        using operand_type = ir::node_data<double>;
+        using operands_type = std::vector<operand_type>;
+
+        hpx::future<primitive_argument_type> eval(
+            std::vector<primitive_argument_type> const& operands,
+            std::vector<primitive_argument_type> const& args) const;
+
     public:
         static match_pattern_type const match_data;
 
@@ -29,6 +41,10 @@ namespace phylanx { namespace execution_tree { namespace primitives
 
         hpx::future<primitive_argument_type> eval(
             std::vector<primitive_argument_type> const& args) const override;
+
+    private:
+        primitive_argument_type inverse0d(operands_type && ops) const;
+        primitive_argument_type inverse2d(operands_type && ops) const;
     };
 
     PHYLANX_EXPORT primitive create_inverse_operation(
