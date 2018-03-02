74ac0017dda0c3b25a705fb0e303ac381f434a63
Parsa Amini <parsa@cct.lsu.edu>
2018-03-02 16:17:53 -0600

    Refactored linearmatrix primitive

--- phylanx/execution_tree/primitives/linearmatrix.hpp
+++ phylanx/execution_tree/primitives/linearmatrix.hpp
@@ -1,7 +1,7 @@
-//   Copyright (c) 2018 R. Tohid
+// Copyright (c) 2018 R. Tohid
 //
-//   Distributed under the Boost Software License, Version 1.0. (See accompanying
-//   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(PHYLANX_PRIMITIVES_LINEARMATRIX_FEB_04_2018_0331PM)
 #define PHYLANX_PRIMITIVES_LINEARMATRIX_FEB_04_2018_0331PM
@@ -9,16 +9,29 @@
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/base_primitive.hpp>
 #include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/ir/node_data.hpp>
 
 #include <hpx/lcos/future.hpp>
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace phylanx { namespace execution_tree { namespace primitives
 {
-    class linearmatrix : public primitive_component_base
+    class linearmatrix
+        : public primitive_component_base
+        , public std::enable_shared_from_this<linearmatrix>
     {
+    protected:
+        using arg_type = ir::node_data<double>;
+        using args_type = std::vector<arg_type>;
+        using matrix_type = blaze::DynamicMatrix<double>;
+
+        hpx::future<primitive_argument_type> eval(
+            std::vector<primitive_argument_type> const& operands,
+            std::vector<primitive_argument_type> const& args) const;
+
     public:
         static match_pattern_type const match_data;
 
@@ -29,6 +42,9 @@ namespace phylanx { namespace execution_tree { namespace primitives
 
         hpx::future<primitive_argument_type> eval(
             std::vector<primitive_argument_type> const& args) const override;
+
+    private:
+        primitive_argument_type linmatrix(args_type&& args) const;
     };
 
     PHYLANX_EXPORT primitive create_linearmatrix(hpx::id_type const& locality,
--- src/execution_tree/primitives/linearmatrix.cpp
+++ src/execution_tree/primitives/linearmatrix.cpp
@@ -1,7 +1,7 @@
-//  Copyright (c) 2018 R. Tohid
+// Copyright (c) 2018 R. Tohid
 //
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/linearmatrix.hpp>
@@ -47,130 +47,107 @@ namespace phylanx { namespace execution_tree { namespace primitives
     {}
 
     ///////////////////////////////////////////////////////////////////////////
-    namespace detail
+    primitive_argument_type linearmatrix::linmatrix(args_type&& args) const
     {
-        struct linearmatrix : std::enable_shared_from_this<linearmatrix>
+
+        if (5 != args.size())
         {
-            linearmatrix(std::string const& name, std::string const& codename)
-              : name_(name)
-              , codename_(codename)
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "linearmatrix::linmatrix",
+                execution_tree::generate_error_message(
+                    "linearmatrix primitive requires five "
+                        "arguments.",
+                    name_, codename_));
+        }
+
+        std::size_t nx = extract_integer_value(args[0]);
+        std::size_t ny = extract_integer_value(args[1]);
+        double base_value = args[2].scalar();
+        double dx = args[3].scalar();
+        double dy = args[4].scalar();
+
+        if (nx < 1)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "phylanx::execution_tree::primitives::"
+                    "detail::linearmatrix",
+                execution_tree::generate_error_message(
+                    "the size of matrix in dimension x must at "
+                        "least be one.",
+                    name_, codename_));
+        }
+
+        if (ny < 1)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "phylanx::execution_tree::primitives::"
+                    "detail::linearmatrix",
+                execution_tree::generate_error_message(
+                    "the size of matrix in dimension y must at "
+                        "least be one.",
+                    name_, codename_));
+        }
+
+        matrix_type result{nx, ny};
+
+        for(int x = 0; x < nx; x++)
+        {
+            for(int y = 0; y < ny; y++)
             {
+                result(x, y) = base_value + dx * x + dy * y;
             }
+        }
 
-        protected:
-            std::string name_;
-            std::string codename_;
-
-        protected:
-            using arg_type = ir::node_data<double>;
-            using args_type = std::vector<arg_type>;
-            using matrix_type = blaze::DynamicMatrix<double>;
-
-            primitive_argument_type linmatrix(args_type&& args) const
-            {
-
-                if (5 != args.size())
-                {
-                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                        "linearmatrix::linmatrix",
-                        generate_error_message(
-                            "linearmatrix primitive requires five "
-                                "arguments.",
-                            name_, codename_));
-                }
-
-                std::size_t nx = extract_integer_value(args[0]);
-                std::size_t ny = extract_integer_value(args[1]);
-                double base_value = args[2].scalar();
-                double dx = args[3].scalar();
-                double dy = args[4].scalar();
-
-                if (nx < 1)
-                {
-                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                        "phylanx::execution_tree::primitives::"
-                            "detail::linearmatrix",
-                        generate_error_message(
-                            "the size of matrix in dimension x must at "
-                                "least be one.",
-                            name_, codename_));
-                }
-
-                if (ny < 1)
-                {
-                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                        "phylanx::execution_tree::primitives::"
-                            "detail::linearmatrix",
-                        generate_error_message(
-                            "the size of matrix in dimension y must at "
-                                "least be one.",
-                            name_, codename_));
-                }
-
-                matrix_type result{nx, ny};
-
-                for(int x = 0; x < nx; x++)
-                {
-                    for(int y = 0; y < ny; y++)
-                    {
-                        result(x, y) = base_value + dx * x + dy * y;
-                    }
-                }
-
-                return arg_type{std::move(result)};
-            }
-
-        public:
-            hpx::future<primitive_argument_type> eval(
-                std::vector<primitive_argument_type> const& operands,
-                std::vector<primitive_argument_type> const& args)
-            {
-                if (operands.size() != 5)
-                {
-                    HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                        "phylanx::execution_tree::primitives::"
-                            "linearmatrix",
-                        generate_error_message(
-                            "the linearmatrix primitive requires exactly "
-                                "five arguments.",
-                            name_, codename_));
-                }
-
-                for (std::size_t i = 0; i != operands.size(); ++i)
-                {
-                    if (!valid(operands[i]))
-                    {
-                        HPX_THROW_EXCEPTION(hpx::bad_parameter,
-                            "linearmatrix::eval",
-                            generate_error_message(
-                                "at least one of the arguments passed to "
-                                    "linearmatrix is not valid.",
-                                name_, codename_));
-                    }
-                }
-
-                auto this_ = this->shared_from_this();
-                return hpx::dataflow(hpx::util::unwrapping(
-                    [this_](args_type&& args) -> primitive_argument_type
-                    {
-                        return this_->linmatrix(std::move(args));
-                    }),
-                    detail::map_operands(
-                        operands, functional::numeric_operand{}, args,
-                        name_, codename_));
-            }
-        };
+        return arg_type{std::move(result)};
     }
 
+    hpx::future<primitive_argument_type> linearmatrix::eval(
+        std::vector<primitive_argument_type> const& operands,
+        std::vector<primitive_argument_type> const& args) const
+    {
+        if (operands.size() != 5)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "phylanx::execution_tree::primitives::"
+                    "linearmatrix",
+                execution_tree::generate_error_message(
+                    "the linearmatrix primitive requires exactly "
+                        "five arguments.",
+                    name_, codename_));
+        }
+
+        for (std::size_t i = 0; i != operands.size(); ++i)
+        {
+            if (!valid(operands[i]))
+            {
+                HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                    "linearmatrix::eval",
+                    execution_tree::generate_error_message(
+                        "at least one of the arguments passed to "
+                            "linearmatrix is not valid.",
+                        name_, codename_));
+            }
+        }
+
+        auto this_ = this->shared_from_this();
+        return hpx::dataflow(hpx::util::unwrapping(
+            [this_](args_type&& args) -> primitive_argument_type
+            {
+                return this_->linmatrix(std::move(args));
+            }),
+            detail::map_operands(
+                operands, functional::numeric_operand{}, args,
+                name_, codename_));
+    }
+
+    //////////////////////////////////////////////////////////////////////////
     hpx::future<primitive_argument_type> linearmatrix::eval(
         std::vector<primitive_argument_type> const& args) const
     {
         if (operands_.empty())
         {
-            return std::make_shared<detail::linearmatrix>(name_, codename_)
-                ->eval(args, noargs);
+            return eval(args, noargs);
         }
-        return std::make_shared<detail::linearmatrix>(name_, codename_)
-            ->eval(operands_, args);
+        return eval(operands_, args);
     }
 }}}
