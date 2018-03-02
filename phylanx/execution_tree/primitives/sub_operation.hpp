1ecf908b9ba4107fca978ea69cc1e8307bd73b7b
Parsa Amini <parsa@cct.lsu.edu>
2018-03-02 17:27:22 -0600

    Refactored sub_operation primitive

--- phylanx/execution_tree/primitives/sub_operation.hpp
+++ phylanx/execution_tree/primitives/sub_operation.hpp
@@ -1,7 +1,7 @@
-//  Copyright (c) 2017 Bibek Wagle
+// Copyright (c) 2017 Bibek Wagle
 //
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(PHYLANX_PRIMITIVES_SUB_OPERATION_SEP_15_2017_1035AM)
 #define PHYLANX_PRIMITIVES_SUB_OPERATION_SEP_15_2017_1035AM
@@ -9,16 +9,28 @@
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/base_primitive.hpp>
 #include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/ir/node_data.hpp>
 
 #include <hpx/lcos/future.hpp>
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace phylanx { namespace execution_tree { namespace primitives
 {
-    class sub_operation : public primitive_component_base
+    class sub_operation
+        : public primitive_component_base
+        , public std::enable_shared_from_this<sub_operation>
     {
+    protected:
+        using operand_type = ir::node_data<double>;
+        using operands_type = std::vector<operand_type>;
+
+        hpx::future<primitive_argument_type> eval(
+            std::vector<primitive_argument_type> const& operands,
+            std::vector<primitive_argument_type> const& args) const;
+
     public:
         static match_pattern_type const match_data;
 
@@ -29,6 +41,25 @@ namespace phylanx { namespace execution_tree { namespace primitives
 
         hpx::future<primitive_argument_type> eval(
             std::vector<primitive_argument_type> const& args) const override;
+
+    private:
+        struct sub0dnd_simd;
+        struct subnd0d_simd;
+
+        primitive_argument_type sub0d0d(operands_type && ops) const;
+        primitive_argument_type sub0d1d(operands_type && ops) const;
+        primitive_argument_type sub0d2d(operands_type && ops) const;
+        primitive_argument_type sub0d(operands_type && ops) const;
+        ///////////////////////////////////////////////////////////
+        primitive_argument_type sub1d0d(operands_type && ops) const;
+        primitive_argument_type sub1d1d(operands_type && ops) const;
+        primitive_argument_type sub1d2d(operands_type&& ops) const;
+        primitive_argument_type sub1d(operands_type && ops) const;
+        ///////////////////////////////////////////////////////////
+        primitive_argument_type sub2d0d(operands_type && ops) const;
+        primitive_argument_type sub2d1d(operands_type&& ops) const;
+        primitive_argument_type sub2d2d(operands_type && ops) const;
+        primitive_argument_type sub2d(operands_type && ops) const;
     };
 
     PHYLANX_EXPORT primitive create_sub_operation(hpx::id_type const& locality,
