4280284e506acc14a7b6f586a6fb29807679e942
Parsa Amini <parsa@cct.lsu.edu>
2018-03-02 08:22:25 -0600

    Refactored identity primitive

--- phylanx/execution_tree/primitives/identity.hpp
+++ phylanx/execution_tree/primitives/identity.hpp
@@ -9,16 +9,29 @@
 #include <phylanx/config.hpp>
 #include <phylanx/execution_tree/primitives/base_primitive.hpp>
 #include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/ir/node_data.hpp>
 
 #include <hpx/lcos/future.hpp>
 
+#include <memory>
 #include <string>
 #include <vector>
 
 namespace phylanx { namespace execution_tree { namespace primitives
 {
-    class identity : public primitive_component_base
+    class identity
+        : public primitive_component_base
+        , public std::enable_shared_from_this<identity>
     {
+    protected:
+        using operand_type = ir::node_data<double>;
+        using matrix_type = blaze::IdentityMatrix<double>;
+        using operands_type = std::vector<operand_type>;
+
+        hpx::future<primitive_argument_type> eval(
+            std::vector<primitive_argument_type> const& operands,
+            std::vector<primitive_argument_type> const& args) const;
+
     public:
         static match_pattern_type const match_data;
 
@@ -29,6 +42,9 @@ namespace phylanx { namespace execution_tree { namespace primitives
 
         hpx::future<primitive_argument_type> eval(
             std::vector<primitive_argument_type> const& args) const override;
+
+    private:
+        primitive_argument_type identity_nd(operands_type&& ops) const;
     };
 
     PHYLANX_EXPORT primitive create_identity(hpx::id_type const& locality,
