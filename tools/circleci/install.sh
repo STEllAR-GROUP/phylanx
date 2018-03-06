a939955e3d106b9d80f777e5ea5f52f0fa7e9f95
Parsa Amini <me@parsaamini.net>
2018-03-06 17:17:53 -0600

    Install to default paths

--- tools/circleci/install.sh
+++ tools/circleci/install.sh
@@ -4,6 +4,11 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+# This script is to be used on CircleCI for the purpose of installing Phylanx
+# to the Docker image
+set -x
+
 mkdir -p /phylanx
-cp -a /.data /phylanx
+cd /.data
+cp -a . /phylanx
 make -C /phylanx/build install
