f7f0a85a0e89fda6d0d44b4e75e73df9893551f0
Parsa Amini <parsa@cct.lsu.edu>
2018-03-21 07:47:26 -0500

    Added range support to base_primitive

--- phylanx/execution_tree/primitives/base_primitive.hpp
+++ phylanx/execution_tree/primitives/base_primitive.hpp
@@ -1,8 +1,8 @@
-//  Copyright (c) 2017-2018 Hartmut Kaiser
-//  Copyright (c) 2018 Parsa Amini
-//
-//  Distributed under the Boost Software License, Version 1.0. (See accompanying
-//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Copyright (c) 2017-2018 Hartmut Kaiser
+// Copyright (c) 2018 Parsa Amini
+// 
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if !defined(PHYLANX_PRIMITIVES_BASE_PRIMITIVE_SEP_05_2017_1102AM)
 #define PHYLANX_PRIMITIVES_BASE_PRIMITIVE_SEP_05_2017_1102AM
