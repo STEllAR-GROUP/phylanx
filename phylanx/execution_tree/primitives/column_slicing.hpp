565692da2e1be6d8fdd940b5db574539193c941b
Bibek Wagle <justwagle@gmail.com>
2018-04-04 17:07:00 -0500

    Changing Column Slicing to accept lists

--- phylanx/execution_tree/primitives/column_slicing.hpp
+++ phylanx/execution_tree/primitives/column_slicing.hpp
@@ -70,9 +70,12 @@ namespace phylanx { namespace execution_tree { namespace primitives
     private:
         std::vector<int> create_list(
             int start, int stop, int step, int array_length) const;
-        primitive_argument_type column_slicing0d(args_type&& args) const;
-        primitive_argument_type column_slicing1d(args_type&& args) const;
-        primitive_argument_type column_slicing2d(args_type&& args) const;
+      primitive_argument_type column_slicing0d(
+          arg_type&& arg, std::vector<double> extracted) const;
+      primitive_argument_type column_slicing1d(
+          arg_type&& arg, std::vector<double> extracted) const;
+      primitive_argument_type column_slicing2d(
+          arg_type&& arg, std::vector<double> extracted) const;
     };
 
     PHYLANX_EXPORT primitive create_column_slicing_operation(
