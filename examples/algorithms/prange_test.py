921c00c990baaae69eb83349fae463e885892a71
ct-clmsn <ct-clmsn(at)gmail>
2018-05-24 11:55:25 -0400

    test for prange

---
+++ examples/algorithms/prange_test.py
@@ -0,0 +1,14 @@
+#  Copyright (c) 2018 Christopher Taylor
+#
+#  Distributed under the Boost Software License, Version 1.0. (See accompanying
+#  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+import phylanx
+from phylanx.ast import *
+
+@Phylanx(debug=True)
+def test_prange():
+    for x in prange(0,10):
+        print (x)
+
+test_prange()
