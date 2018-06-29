e5d6986cfe52560488ede0e8cf916fee0aa2edfd
ct-clmsn <ct-clmsn(at)gmail>
2018-06-29 14:51:13 -0400

    added self-ref update

--- tests/regressions/python/sum_stat_perf_test.py
+++ tests/regressions/python/sum_stat_perf_test.py
@@ -32,13 +32,52 @@ def phy_run_mat(y, k):
     for i in range(k):
         x[y[i], y[i]] += 1.0
 
+def py_run_self_update(y, k):
+    x = np.zeros(k)
+    for i in range(k):
+        x[y[i]] = x[y[i]] + 1.0
+
+@Phylanx
+def phy_run_self_update(y, k):
+    x = constant(0.0, k)
+    for i in range(k):
+        x[y[i]] = x[y[i]] + 1.0
+
+def py_run_mat_self_update(y, k):
+    x = np.zeros((k,k))
+    for i in range(k):
+        x[y[i], y[i]] = x[y[i], y[i]] + 1.0
+
+@Phylanx
+def phy_run_mat_self_update(y, k):
+    x = constant(0.0, make_list(k, k))
+    for i in range(k):
+        x[y[i], y[i]] = x[y[i], y[i]] + 1.0
+
 
 if __name__ == "__main__":
     K = 10000 # add a 0
     y = np.random.randint(0, K, size=K)
 
-    func = [ py_run_mat, phy_run_mat, py_run, phy_run ]
-    func_nom = [ 'py_run_mat', 'phy_run_mat', 'py_run', 'phy_run' ]
+    func = [ py_run_mat
+           , phy_run_mat
+           , py_run
+           , phy_run
+           , py_run_self_update
+           , phy_run_self_update
+           , py_run_mat_self_update
+           , phy_run_mat_self_update
+    ]
+
+    func_nom = [ 'py_run_mat'
+               , 'phy_run_mat'
+               , 'py_run'
+               , 'phy_run'
+               , 'py_run_self_update'
+               , 'phy_run_self_update'
+               , 'py_run_mat_self_update'
+               , 'phy_run_mat_self_update'
+    ]
 
     for n, f in zip(func_nom, func):
         print(n)
