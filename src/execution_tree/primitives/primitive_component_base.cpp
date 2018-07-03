d1df815d7bf1d41d9a53cea58c0d53f2d4936e21
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2018-07-03 11:30:06 -0500

    Revert an accidentally committed change

--- src/execution_tree/primitives/primitive_component_base.cpp
+++ src/execution_tree/primitives/primitive_component_base.cpp
@@ -185,32 +185,31 @@ namespace phylanx { namespace execution_tree { namespace primitives
     hpx::launch primitive_component_base::select_direct_eval_execution(
         hpx::launch policy) const
     {
-//         if (eval_count_ != 0)
-//         {
-//             // check whether execution status needs to be changed (with some
-//             // hysteresis)
-//             std::int64_t exec_time = (eval_duration_ / eval_count_);
-//             if (exec_time > 300000)
-//             {
-//                 execute_directly_ = 0;
-//             }
-//             else if (exec_time < 150000)
-//             {
-//                 execute_directly_ = 1;
-//             }
-//         }
-//
-//         if (execute_directly_ == 1)
-//         {
-//             return hpx::launch::sync;
-//         }
-//         else if (execute_directly_ == 0)
-//         {
-//             return hpx::launch::async;
-//         }
-//
-//         return policy;
-        return hpx::launch::sync;
+        if (eval_count_ != 0)
+        {
+            // check whether execution status needs to be changed (with some
+            // hysteresis)
+            std::int64_t exec_time = (eval_duration_ / eval_count_);
+            if (exec_time > 300000)
+            {
+                execute_directly_ = 0;
+            }
+            else if (exec_time < 150000)
+            {
+                execute_directly_ = 1;
+            }
+        }
+
+        if (execute_directly_ == 1)
+        {
+            return hpx::launch::sync;
+        }
+        else if (execute_directly_ == 0)
+        {
+            return hpx::launch::async;
+        }
+
+        return policy;
     }
 }}}
 
