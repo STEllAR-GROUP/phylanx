7d0feb91cc1509d622f6a7baf039af68faea55a9
ct-clmsn <ct-clmsn(at)gmail>
2018-07-31 22:29:18 -0400

    renamed defines

--- phylanx/plugins/fileio/csc_read_hdf5.hpp
+++ phylanx/plugins/fileio/csc_read_hdf5.hpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
-#ifndef __CSCHDF5READ__
-#define __CSCHDF5READ__
+#ifndef __CSCREADHDF5__
+#define __CSCREADHDF5__
 
 #include <phylanx/config.hpp>
 
--- phylanx/plugins/fileio/csc_write_hdf5.hpp
+++ phylanx/plugins/fileio/csc_write_hdf5.hpp
@@ -4,8 +4,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #pragma once
-#ifndef __CSCHDF5WRITE__
-#define __CSCHDF5WRITE__
+#ifndef __CSCWRITEHDF5__
+#define __CSCWRITEHDF5__
 
 #include <phylanx/config.hpp>
 
