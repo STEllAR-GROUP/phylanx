2aa23f9339bb223c4778da1b26672cca7c6909de
Shahrzad Shirzad <shahrzadshirzad@gmail.com>
2018-09-10 10:21:15 -0500

    updating ALS algorithm codes

--- examples/algorithms/als/phylanx_als_csv.py
+++ examples/algorithms/als/phylanx_als_csv.py
@@ -3,67 +3,91 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-import phylanx
-from phylanx.ast import *
+from phylanx import Phylanx
 import numpy as np
 
 
 @Phylanx
-def ALS(filename, row_stop, col_stop, regularization, num_factors, iterations, alpha,
-        enable_output):
-    data = file_read_csv(filename)
-    ratings = data[0:row_stop, 0:col_stop]
-    num_users = shape(ratings, 0)
-    num_items = shape(ratings, 1)
+def ALS(ratings, regularization, num_factors, iterations, alpha, enable_output):
+    num_users = np.shape(ratings)[0]
+    num_items = np.shape(ratings)[1]
+
     conf = alpha * ratings
-    conf_u = constant(0.0, make_list(num_items))
-    conf_i = constant(0.0, make_list(num_users))
-    c_u = constant(0.0, make_list(num_items, num_items))
-    c_i = constant(0.0, make_list(num_users, num_users))
-    p_u = constant(0.0, make_list(num_items))
-    p_i = constant(0.0, make_list(num_users))
-    set_seed(0)
-    X = random(make_list(num_users, num_factors))
-    Y = random(make_list(num_items, num_factors))
-    I_f = identity(num_factors)
-    I_i = identity(num_items)
-    I_u = identity(num_users)
+    conf_u = np.zeros((num_items, 1))
+    conf_i = np.zeros((num_items, 1))
+
+    c_u = np.zeros((num_items, num_items))
+    c_i = np.zeros((num_users, num_users))
+    p_u = np.zeros((num_items, 1))
+    p_i = np.zeros((num_users, 1))
+
+    I_f = np.identity(num_factors)
+    I_i = np.identity(num_items)
+    I_u = np.identity(num_users)
+
+    set_seed(0)  # noqa: F821
+    X = random([num_users, num_factors])  # noqa: F821
+    Y = random([num_items, num_factors])  # noqa: F821
+
     i = 0
     u = 0
     k = 0
-    XtX = constant(0.0, make_list(num_factors, num_factors))
-    YtY = constant(0.0, make_list(num_factors, num_factors))
-    A = constant(0.0, make_list(num_factors, num_factors))
-    b = constant(0.0, make_list(num_factors))
+
+    XtX = np.zeros((num_factors, num_factors))
+    YtY = np.zeros((num_factors, num_factors))
+    A = np.zeros([num_factors, num_factors])
+    b = np.zeros([num_factors])
     while k < iterations:
         if enable_output:
-            print("iteration: ", k)
-            print("X :", X)
-            print("Y :", Y)
-        YtY = dot(transpose(Y), Y) + regularization * I_f
-        XtX = dot(transpose(X), X) + regularization * I_f
+            print("iteration ", k)
+            print("X: ", X)
+            print("Y: ", Y)
+        YtY = np.dot(np.transpose(Y), Y) + regularization * I_f
+        XtX = np.dot(np.transpose(X), X) + regularization * I_f
         while u < num_users:
-            conf_u = slice_row(conf, u)
-            c_u = diag(conf_u)
+            conf_u = conf[u, :]
+            c_u = np.diag(conf_u)
             p_u = conf_u != 0
-            A = YtY + dot(dot(transpose(Y), c_u), Y)
-            b = dot(dot(transpose(Y), c_u + I_i), transpose(p_u))
-            set_row(X, u, u + 1, 1, dot(inverse(A), b))
+            A = YtY + np.dot(np.dot(np.transpose(Y), c_u), Y)
+            b = np.dot(np.dot(np.transpose(Y), c_u + I_i), np.transpose(p_u))
+            X[u, :] = np.dot(inverse(A), b)  # noqa: F821
             u = u + 1
         while i < num_items:
-            conf_i = slice_column(conf, i)
-            c_i = diag(conf_i)
+            conf_i = conf[:, i]
+            c_i = np.diag(conf_i)
             p_i = conf_i != 0
-            A = XtX + dot(dot(transpose(X), c_i), X)
-            b = dot(dot(transpose(X), c_i + I_u), transpose(p_i))
-            set_row(Y, i, i + 1, 1, dot(inverse(A), b))
+            A = XtX + np.dot(np.dot(np.transpose(X), c_i), X)
+            b = np.dot(np.dot(np.transpose(X), c_i + I_u), np.transpose(p_i))
+            Y[i, :] = np.dot(inverse(A), b)  # noqa: F821
             i = i + 1
         u = 0
         i = 0
         k = k + 1
-    return make_list(X, Y)
+    result = np.vstack((X, Y))
+    return result
 
 
-(a, b) = ALS("MovieLens.csv", 10, 10, 0.1, 3, 1, 40, True)
-print(a)
-print(b)
+# test example
+ratings = np.zeros((10, 5))
+ratings[0, 1] = 4
+ratings[1, 0] = 1
+ratings[1, 2] = 4
+ratings[1, 4] = 5
+ratings[2, 3] = 2
+ratings[3, 1] = 8
+ratings[4, 2] = 4
+ratings[6, 4] = 2
+ratings[7, 0] = 1
+ratings[8, 3] = 5
+ratings[9, 0] = 1
+ratings[9, 3] = 2
+
+regularization = 0.1
+num_factors = 3
+iterations = 5
+alpha = 40
+enable_output = False
+
+result = ALS(ratings, regularization, num_factors, iterations, alpha, enable_output)
+print(" X = ", result[0:np.shape(ratings)[0], :])
+print(" Y = ", result[0:np.shape(ratings)[1], :])
