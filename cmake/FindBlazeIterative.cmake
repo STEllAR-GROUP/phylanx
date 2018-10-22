609aaa2e9e094da3006b5382037ed44fd0e80d85
Patrick Diehl <me@diehlpk.de>
2018-10-22 10:06:37 -0500

    Add QUIET option to the find_package

--- cmake/FindBlazeIterative.cmake
+++ cmake/FindBlazeIterative.cmake
@@ -2,7 +2,7 @@
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-find_package(PkgConfig)
+find_package(PkgConfig QUIET)
 
 find_path(BLAZEITERATIVE_INCLUDE BlazeIterative/BlazeIterative.hpp HINTS /usr/include /usr/local/include  "${BLAZEITERATIVE_DIR}/include/" )
 
