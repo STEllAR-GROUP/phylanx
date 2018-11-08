5f324578cfa6db66cc52d55ab16fdbb9dccca4f5
Bita Hasheminezhad <bita.hn90@gmail.com>
2018-11-08 15:54:42 -0600

    Squeeze primitive added

---
+++ phylanx/plugins/matrixops/squeeze_operation.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2018 Hartmut Kaiser
+// Copyright (c) 2018 Bita Hasheminezhad
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(PHYLANX_PLUGINS_MATRIXOPS_SQUEEZE_OPERATION)
+#define PHYLANX_PLUGINS_MATRIXOPS_SQUEEZE_OPERATION
+
+
+
+#endif 
---
+++ tests/unit/plugins/matrixops/squeeze_operation.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2018 Hartmut Kaiser
+// Copyright (c) 2018 Bita Hasheminezhad
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/phylanx.hpp>
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/lcos.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+int main(int argc, char* argv[])
+{
+    return hpx::util::report_errors();
+}
