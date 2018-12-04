4f01400ae10b73b31f9ad5e7038bf37c00217272
ct-clmsn <ct-clmsn(at)gmail>
2018-12-04 18:09:40 -0500

    recompile test

---
+++ phylanx/plugins/algorithms/impl/randomforest.hpp
@@ -0,0 +1,454 @@
+//  Copyright (c) 2018 Christopher Taylor
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <hpx/throw_exception.hpp>
+#include <hpx/parallel/algorithms/for_each.hpp>
+#include <hpx/parallel/algorithms/transform.hpp>
+#include <hpx/parallel/algorithms/reduce.hpp>
+
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/get.hpp>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/range/irange.hpp>
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+#include <map>
+#include <cmath>
+
+#include <blaze/Math.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+  life savers...
+
+   https://www.boost.org/doc/libs/1_68_0/libs/spirit/doc/html/spirit/qi/tutorials/mini_xml___asts_.html
+   https://www.boost.org/doc/libs/1_68_0/libs/spirit/example/qi/mini_xml1.cpp
+ */
+namespace phylanx { namespace algorithms { namespace impl {
+
+struct nil {};
+struct randomforest_node;
+
+using randomforest_node_variant = boost::variant<
+    nil
+    , boost::recursive_wrapper<randomforest_node>
+    , std::uint64_t
+    , double
+    , std::tuple< std::vector< std::uint64_t >, std::vector< std::uint64_t > >
+>;
+
+using randomforest_node_map = std::map<
+    std::string, randomforest_node_variant
+>;
+
+struct randomforest_node {
+    randomforest_node_map fields;
+};
+
+}}} // end namespace
+
+BOOST_FUSION_ADAPT_STRUCT(
+    phylanx::algorithms::impl::randomforest_node,
+    (phylanx::algorithms::impl::randomforest_node_map, fields)
+)
+
+namespace phylanx { namespace algorithms { namespace impl {
+
+struct randomforest_impl {
+
+    std::vector<randomforest_node> trees;
+    std::unordered_map<double, std::uint64_t> classes;
+
+    randomforest_impl(std::uint64_t const ntrees)
+        : trees(ntrees)
+        , classes() {
+    }
+
+    static void test_split(
+            std::uint64_t const feature
+            , double const val
+            , blaze::DynamicMatrix<double> const& dataset
+            , std::tuple< std::vector< std::uint64_t >, std::vector< std::uint64_t > > & groups ) {
+
+        std::vector< std::uint64_t > left, right;
+        auto const rows = dataset.rows();
+
+        for(auto rowidx = 0; rowidx < rows; ++rowidx) {
+            if(dataset(rowidx, feature) < val) {
+                left.push_back(rowidx);
+            }
+            else {
+                right.push_back(rowidx);
+            }
+        }
+
+        groups = std::make_tuple(left, right);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static double gini_index(
+        blaze::DynamicMatrix<double> const& dataset
+        , blaze::DynamicVector<double> const& dataset_labels
+        , std::tuple< std::vector<std::uint64_t>, std::vector<std::uint64_t> > const& groups
+        , std::unordered_map<double, std::uint64_t> & classes) {
+
+        std::vector<std::uint64_t> groups_len{std::get<0>(groups).size(), std::get<1>(groups).size()};
+        std::vector< std::vector<std::uint64_t> > groups_vec{std::get<0>(groups), std::get<1>(groups)};
+
+        double const n_instances = static_cast<double>(std::accumulate(groups_len.begin(), groups_len.end(), 0UL));
+        double gini = 0.0;
+
+        blaze::DynamicVector<double> p(classes.size());
+
+        for(auto i = 0; i < groups_len.size(); ++i) {
+            if(groups_len[i] < 1) { continue; }
+            double const& group_len = static_cast<double>(groups_len[i]);
+            auto const& group = groups_vec[i];
+
+            for(auto const& row_idx : group) {
+                p[classes[dataset_labels[row_idx]]] += 1.0;
+            }
+
+            double const score = std::accumulate(p.begin(), p.end(), 0.0
+                , [&group_len](double const pval, double const qval) {
+                    return std::pow(pval/group_len, 2.0) + std::pow(qval/group_len, 2.0);
+                }
+            );
+
+            gini += (1.0 - score) * static_cast<double>(group_len / n_instances);
+            std::transform(p.begin(), p.end(), p.begin(), [](auto const i) { return 0.0; });
+        }
+
+        return gini;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename T, typename F>
+    static void argsort(std::vector<T> const& v, std::vector<F> & idx ) {
+        if(idx.size() < v.size())
+            idx.resize(v.size());
+
+        std::iota(idx.begin(), idx.end(), 0);
+
+        std::sort(idx.begin(), idx.end()
+            , [&v](F const i1, F const i2) {
+                return v[i1] < v[i2];
+            }
+        );
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static void get_split(
+        blaze::DynamicMatrix<double> const& dataset
+        , blaze::DynamicVector<double> const& dataset_labels
+        , std::vector<std::uint64_t> const& dataset_indices
+        , std::uint64_t const n_features
+        , std::unordered_map<double, std::uint64_t> & classes
+        , randomforest_node & ret) {
+
+        auto b_idx = std::numeric_limits<std::uint64_t>::max();
+        auto b_val = std::numeric_limits<double>::max();
+        auto b_score = std::numeric_limits<double>::max();
+        std::tuple< std::vector<std::uint64_t>, std::vector<std::uint64_t> > b_groups;
+
+        std::vector<std::uint64_t> features(n_features);
+
+        {
+            std::default_random_engine generator;
+            std::uniform_int_distribution<std::uint64_t> distribution(0, dataset.columns());
+            auto gen = [&generator, &distribution]() { return distribution(generator); };
+
+            std::vector<std::int64_t> idx_w(dataset.columns());
+            std::generate_n(idx_w.begin(), idx_w.size(), [&gen]() { return gen(); });
+            std::vector<std::uint64_t> idx_w_sort;
+            argsort(idx_w, idx_w_sort);
+
+            std::vector<std::uint64_t> idx(idx_w_sort.size());
+            std::iota(idx.begin(), idx.end(), 0);
+
+            std::transform(
+                idx_w_sort.begin(), idx_w_sort.begin()+n_features
+                , features.begin()
+                , [&idx](auto const i) {
+                    return idx[i];
+                }
+            );
+        }
+
+        std::tuple< std::vector< std::uint64_t >, std::vector< std::uint64_t > > groups;
+
+        for(auto feature : features) {
+            for(auto const r : dataset_indices) {
+                test_split(feature, dataset(r, feature), dataset, groups);
+                auto const gini = gini_index(dataset, dataset_labels, groups, classes);
+                if(gini < b_score) {
+                    b_idx = feature;
+                    b_val = dataset(r, feature);
+                    b_score = gini;
+                    b_groups = std::make_tuple( std::get<0>(groups), std::get<1>(groups) );
+                }
+            }
+        }
+
+        ret.fields["index"] = b_idx;
+        ret.fields["value"] = b_val;
+        ret.fields["groups"] = b_groups;
+        ret.fields["lw"] = std::numeric_limits<std::uint64_t>::max();
+        ret.fields["rw"] = std::numeric_limits<std::uint64_t>::max();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static std::uint64_t to_terminal(
+        blaze::DynamicVector<double> const& train_labels
+        , std::vector<std::uint64_t> const& group
+        , std::unordered_map<double, std::uint64_t> & classes) {
+
+        blaze::DynamicVector<double> outcome_hist(classes.size());
+
+        for(auto g : group) {
+            double const k = train_labels[g];
+            outcome_hist[classes[k]] += 1.0;
+        }
+
+        return std::distance(outcome_hist.begin()
+            , std::max_element(outcome_hist.begin(), outcome_hist.end())
+        );
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static void split(randomforest_node & node
+        , blaze::DynamicMatrix<double> const& train
+        , blaze::DynamicVector<double> const& train_labels
+        , std::uint64_t max_depth
+        , std::uint64_t min_size
+        , std::uint64_t n_features
+        , std::uint64_t depth
+        , std::unordered_map<double, std::uint64_t> & classes) {
+
+        using tuple_vec_vec = std::tuple< std::vector<std::uint64_t>, std::vector<std::uint64_t> >;
+        std::tuple< std::vector<std::uint64_t>, std::vector<std::uint64_t> > left_right = boost::get<tuple_vec_vec>(node.fields["groups"]);
+        node.fields.erase("groups");
+
+        if(std::get<0>(left_right).size() == 0) {
+            auto term = to_terminal(train_labels, std::get<1>(left_right), classes);
+            node.fields["lw"] = term;
+        }
+        else if(std::get<1>(left_right).size() == 0) {
+            auto term = to_terminal(train_labels, std::get<0>(left_right), classes);
+            node.fields["rw"] = term;
+        }
+
+        if(depth >= max_depth) {
+            auto lterm = to_terminal(train_labels, std::get<0>(left_right), classes);
+            auto rterm = to_terminal(train_labels, std::get<1>(left_right), classes);
+            node.fields["lw"] = lterm;
+            node.fields["rw"] = rterm;
+            return;
+        }
+
+        if(std::get<0>(left_right).size() <= min_size) {
+             auto lterm = to_terminal(train_labels, std::get<0>(left_right), classes);
+             node.fields["lw"] = lterm;
+        }
+
+        randomforest_node left_node;
+        get_split(train, train_labels, std::get<0>(left_right), n_features, classes, left_node);
+        node.fields["left"] = left_node;
+        split(boost::get<randomforest_node>(node.fields["left"]), train, train_labels, max_depth, min_size, n_features, depth + 1, classes);
+        
+        if(std::get<1>(left_right).size() <= min_size) {
+            auto rterm = to_terminal(train_labels, std::get<1>(left_right), classes);
+            node.fields["rw"] = rterm;
+        }
+
+        randomforest_node right_node;
+        get_split(train, train_labels, std::get<1>(left_right), n_features, classes, right_node);
+        node.fields["right"] = right_node;
+        split(boost::get<randomforest_node>(node.fields["right"]), train, train_labels, max_depth, min_size, n_features, depth + 1, classes);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static void build_tree(
+        randomforest_node & tree_root
+        , blaze::DynamicMatrix<double> const& train
+        , blaze::DynamicVector<double> const& train_labels
+        , std::vector<std::uint64_t> const& sample_indices
+        , std::uint64_t max_depth
+        , std::uint64_t min_size
+        , std::uint64_t n_features
+        , std::unordered_map<double, std::uint64_t> & classes) {
+
+        get_split(train, train_labels, sample_indices, n_features, classes, tree_root);
+        split(tree_root, train, train_labels, max_depth, min_size, n_features, 1, classes);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static std::uint64_t node_predict(
+        randomforest_node & node
+        , blaze::DynamicMatrix<double> const& r
+        , std::uint64_t const i) {
+
+        auto index = boost::get<std::uint64_t>(node.fields["index"]);
+        auto value = boost::get<double>(node.fields["value"]);
+
+        if(r(i, index) < value) {
+            auto lw = boost::get<std::uint64_t>(node.fields["lw"]);
+            if( lw == std::numeric_limits<std::uint64_t>::max()) {
+                auto left = boost::get<randomforest_node>(node.fields["left"]);
+                return node_predict(left, r, i);
+            }
+            else {
+                return lw;
+            }
+        } 
+
+        auto rw = boost::get<std::uint64_t>(node.fields["rw"]);
+        if(rw == std::numeric_limits<std::uint64_t>::max()) {
+            auto right = boost::get<randomforest_node>(node.fields["right"]);
+            return node_predict(right, r, i);
+        }
+
+        return rw;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static void subsample(
+        blaze::DynamicMatrix<double> const& dataset
+        , std::vector<std::uint64_t> & idx_w_sort
+        , std::uint64_t const ratio) {
+
+        std::uint64_t const n_sample =
+            static_cast<std::uint64_t>(std::floor(static_cast<double>(
+                dataset.rows()) * ratio)
+        );
+
+        std::default_random_engine generator;
+        std::uniform_int_distribution<std::int64_t> distribution(0,dataset.rows());
+        auto gen = [&generator, &distribution] { return static_cast<std::uint64_t>(distribution(generator)); };
+
+        std::vector<std::uint64_t> idx_w(dataset.rows());
+        std::generate_n(idx_w.begin(), idx_w.size(), gen);
+        argsort(idx_w, idx_w_sort);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    static std::uint64_t bagging_predict(
+        std::vector<randomforest_node> & trees
+        , blaze::DynamicMatrix<double> const& dataset
+        , std::uint64_t const row
+        , std::unordered_map<double, std::uint64_t> & classes) {
+
+        std::vector<std::uint64_t> predictions(trees.size());
+
+        hpx::parallel::transform(
+            hpx::parallel::execution::par
+            , trees.begin()
+            , trees.end()
+            , predictions.begin()
+            , [&dataset, &row](auto & tree) {
+                return node_predict(tree, dataset, row);
+            }
+        );
+
+        std::vector< blaze::DynamicVector<std::uint64_t> > prediction_histograms(predictions.size());
+        auto prediction_indices = boost::irange<std::uint64_t>(0UL, predictions.size());
+
+        hpx::parallel::for_each(
+            hpx::parallel::execution::par
+            , std::begin(prediction_indices)
+            , std::end(prediction_indices)
+            , [&classes, &predictions, &prediction_histograms](auto const i) {
+                prediction_histograms[i].resize(classes.size());
+                prediction_histograms[i][predictions[i]] += 1UL;
+            }
+        );
+
+        blaze::DynamicVector<double> fhistogram(classes.size());
+
+        fhistogram = hpx::parallel::reduce(
+            hpx::parallel::execution::par
+            , prediction_histograms.begin()
+            , prediction_histograms.end()
+            , fhistogram
+            , std::plus<blaze::DynamicVector<double>>()
+        );
+
+        auto itr = std::max_element(fhistogram.begin(), fhistogram.end());
+        return std::distance(fhistogram.begin(), itr);
+    }
+
+    void fit(
+        blaze::DynamicMatrix<double> const& train
+        , blaze::DynamicVector<double> const& train_labels
+        , std::uint64_t const max_depth
+        , std::uint64_t const min_size
+        , std::uint64_t const sample_size) {
+
+        std::vector<double> labels(train_labels.size());
+        std::copy(train_labels.begin(), train_labels.end(), labels.begin());
+        std::sort(labels.begin(), labels.end());
+        auto last = std::unique(labels.begin(), labels.end());
+        labels.erase(last, labels.end());
+
+        std::uint64_t i = 0;
+        for(auto itr = labels.begin(); itr != labels.end(); ++itr, ++i) {
+            classes[(*itr)] = i;
+        }
+
+        std::uint64_t const n_features = static_cast<std::uint64_t>(std::floor(std::sqrt(train.rows())));
+        
+        std::vector< std::vector<std::uint64_t> > subsample_indices(trees.size());
+        
+        auto tree_indices = boost::irange<std::uint64_t>(0, trees.size());
+
+        hpx::parallel::for_each(
+            hpx::parallel::execution::par
+            , std::begin(tree_indices)
+            , std::end(tree_indices)
+            , [this, &train, &train_labels, &subsample_indices, &max_depth, &min_size, &sample_size, &n_features]
+                (std::uint64_t const idx) {
+                    subsample(train, subsample_indices[idx], sample_size);
+                    build_tree(trees[idx], train, train_labels, subsample_indices[idx], max_depth, min_size, n_features, classes); 
+            }
+        );
+    }
+
+    void predict(
+        blaze::DynamicMatrix<double> const& test
+        , blaze::DynamicVector<double> & y) {
+
+        if(y.size() < 1UL) {
+            y.resize(test.rows());
+        }
+
+        blaze::DynamicVector<double> labels(classes.size());
+        for(auto & cls : classes) {
+            labels[cls.second] = cls.first;
+        }
+
+        auto indices = boost::irange<std::uint64_t>(0, test.rows());
+
+        hpx::parallel::for_each(
+            hpx::parallel::execution::par
+            , std::begin(indices)
+            , std::end(indices)
+            , [this, &test, &y, &labels](auto const& idx) {
+                y[idx] = labels[bagging_predict(trees, test, idx, classes)];
+            }
+        );
+    }
+
+};
+
+}}} // end namespace
---
+++ src/plugins/algorithms/randomforest.cpp
@@ -0,0 +1,198 @@
+//  Copyright (c) 2018 Hartmut Kaiser
+//  Copyright (c) 2018 Christopher Taylor
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <phylanx/config.hpp>
+#include <phylanx/plugins/algorithms/randomforest.hpp>
+#include <phylanx/plugins/algorithms/impl/randomforest.hpp>
+#include <phylanx/util/detail/add_simd.hpp>
+#include <phylanx/util/detail/div_simd.hpp>
+
+#include <hpx/include/iostreams.hpp>
+#include <hpx/include/lcos.hpp>
+#include <hpx/include/util.hpp>
+#include <hpx/throw_exception.hpp>
+
+#include <boost/range/irange.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <blaze/Math.h>
+
+using namespace phylanx::algorithms::impl;
+
+///////////////////////////////////////////////////////////////////////////////
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+    static void randomforest_predict(
+        blaze::DynamicMatrix<double> const& train
+        , blaze::DynamicVector<double> const& train_labels
+        , std::uint64_t const max_depth
+        , std::uint64_t const min_size
+        , std::uint64_t const sample_size
+        , std::uint64_t const n_trees
+        , blaze::DynamicMatrix<double> const& test
+        , blaze::DynamicVector<double> & test_labels) {
+
+        randomforest_impl rf(n_trees);
+        rf.fit(train, train_labels, max_depth, min_size, sample_size);
+        rf.predict(test, test_labels);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    match_pattern_type const randomforest::match_data =
+    {
+        hpx::util::make_tuple("randomforest",
+            std::vector<std::string>{
+                "randomforest(_1, _2, _3, _4, _5, _6, _7)",
+            },
+            &create_randomforest, &create_primitive<randomforest>,
+            "training, training_labels, max_depth, min_size, sample_size, n_trees, test\n"
+            "Args:\n"
+            "\n"
+            "    training (matrix) : training features\n"
+            "    training_labels (vector) : training labels\n"
+            "the data\n"
+            "    max_depth (int) : depth of tree splits\n"
+            "    min_size (int) : min size\n"
+            "    sample_size (int) : number of samples per tree\n"
+            "    n_trees (int) : number of trees to train\n"
+            "    test (matrix) : testing data to predict upon\n"
+            "\n"
+            "Returns:\n"
+            "\n"
+            "The Calculated weights"
+            )
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+    randomforest::randomforest(primitive_arguments_type&& operands,
+        std::string const& name, std::string const& codename)
+      : primitive_component_base(std::move(operands), name, codename)
+    {}
+
+    ///////////////////////////////////////////////////////////////////////////
+    primitive_argument_type randomforest::calculate_randomforest(
+        primitive_arguments_type && args) const
+    {
+        // extract arguments
+        auto arg1 = extract_numeric_value(args[0], name_, codename_);
+        if (arg1.num_dimensions() != 2)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter, "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires for the first "
+                    "argument ('training_data') to represent a matrix"));
+        }
+        auto training_data = arg1.matrix();
+
+        auto arg2 = extract_numeric_value(args[1], name_, codename_);
+        if (arg2.num_dimensions() != 1)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter, "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires for the second "
+                    "argument ('training_labels') to represent a vector"));
+        }
+        auto training_labels = arg2.vector();
+
+        // verify correctness of argument dimensions
+        if (training_data.rows() != training_labels.size())
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter, "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires for the number "
+                    "of rows in 'training_data' to be equal to the size of 'training_labels'"));
+        }
+
+        auto max_depth = static_cast<std::uint64_t>(
+            extract_scalar_integer_value(args[2], name_, codename_));
+        auto min_size = static_cast<std::uint64_t>(
+            extract_scalar_integer_value(args[3], name_, codename_));
+        auto sample_size = static_cast<std::uint64_t>(
+            extract_scalar_integer_value(args[4], name_, codename_));
+        auto n_trees = static_cast<std::uint64_t>(
+            extract_scalar_integer_value(args[5], name_, codename_));
+
+        auto arg6 = extract_numeric_value(args[6], name_, codename_);
+        if (arg6.num_dimensions() != 2)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter, "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires for the first "
+                    "argument ('testing') to represent a matrix"));
+        }
+
+        auto testing_data = arg6.matrix();
+
+        using vector_type = ir::node_data<double>::storage1d_type;
+
+        // perform calculations
+        vector_type testing_labels(training_data.rows(), 0);
+
+        randomforest_predict(
+            training_data
+            , training_labels
+            , max_depth
+            , min_size
+            , sample_size
+            , n_trees
+            , testing_data
+            , testing_labels);
+
+        return primitive_argument_type{std::move(testing_labels)};
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    hpx::future<primitive_argument_type> randomforest::eval(
+        primitive_arguments_type const& operands,
+        primitive_arguments_type const& args, eval_context ctx) const
+    {
+        if (operands.size() != 7)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires exactly seven "
+                        "operands"));
+        }
+
+        bool arguments_valid = true;
+        for (std::size_t i = 0; i != operands.size(); ++i)
+        {
+            if (!valid(operands[i]))
+            {
+                arguments_valid = false;
+            }
+        }
+
+        if (!arguments_valid)
+        {
+            HPX_THROW_EXCEPTION(hpx::bad_parameter,
+                "randomforest::eval",
+                generate_error_message(
+                    "the randomforest algorithm primitive requires that the "
+                        "arguments given by the operands array are valid"));
+        }
+
+        auto this_ = this->shared_from_this();
+        return hpx::dataflow(hpx::launch::sync, hpx::util::unwrapping(
+            [this_ = std::move(this_)](primitive_arguments_type && args)
+            ->  primitive_argument_type
+            {
+                return this_->calculate_randomforest(std::move(args));
+            }),
+            detail::map_operands(
+                operands, functional::value_operand{}, args,
+                name_, codename_));
+    }
+
+}}}
