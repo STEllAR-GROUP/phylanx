012e201fe82c6cec027b58ad58871ed5e03b68a6
Shahrzad Shirzad <shahrzadshirzad@gmail.com>
2019-01-10 14:16:18 -0600

    Adding clip primitive

---
+++ phylanx/plugins/matrixops/clip.hpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2018 Shahrzad Shirzad
+// Copyright (c) 2018 Hartmut Kaiser
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(PHYLANX_PRIMITIVES_CLIP)
+#define PHYLANX_PRIMITIVES_CLIP
+
+#include <phylanx/config.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+#include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+
+#include <hpx/lcos/future.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+    /// \brief Implementation of clip as a Phylanx primitive.
+    /// Given an interval, values outside the interval are clipped to the interval edges.
+    /// This implementation is intended to behave like [NumPy implementation of clip]
+    /// (https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.clip.html).
+    /// \param a It may be a scalar value, vector, or matrix
+    /// \param a_min Minimum value, it may be scalar or array_like or None
+    /// \param a_max Maximum valus, it may be scalar or array_like or None
+
+    class clip
+      : public primitive_component_base
+      , public std::enable_shared_from_this<clip>
+    {
+    protected:
+        hpx::future<primitive_argument_type> eval(
+            primitive_arguments_type const& operands,
+            primitive_arguments_type const& args,
+            eval_context ctx) const override;
+
+    public:
+        static match_pattern_type const match_data;
+
+        clip() = default;
+
+        clip(primitive_arguments_type&& operands, std::string const& name,
+            std::string const& codename);
+
+    private:
+        template <typename T>
+        primitive_argument_type clipnd(ir::node_data<T>&& arg,
+            ir::node_data<T>&& min, ir::node_data<T>&& max) const;
+
+        template <typename T>
+        primitive_argument_type clip0d(ir::node_data<T>&& arg,
+            ir::node_data<T>&& min, ir::node_data<T>&& max) const;
+
+        template <typename T>
+        primitive_argument_type clip1d(ir::node_data<T>&& arg,
+            ir::node_data<T>&& min, ir::node_data<T>&& max) const;
+
+        template <typename T>
+        primitive_argument_type clip2d(ir::node_data<T>&& arg,
+            ir::node_data<T>&& min, ir::node_data<T>&& max) const;
+
+        template <typename T>
+        primitive_argument_type clip_helper(
+            primitive_arguments_type&& args) const;
+    };
+
+    inline primitive create_clip(hpx::id_type const& locality,
+        primitive_arguments_type&& operands, std::string const& name = "",
+        std::string const& codename = "")
+    {
+        return create_primitive_component(
+            locality, "clip", std::move(operands), name, codename);
+    }
+}}}
+
+#endif
---
+++ tests/unit/plugins/matrixops/clip.cpp
@@ -0,0 +1,175 @@
+// Copyright (c) 2018 Shahrzad Shirzad
+// Copyright (c) 2018 Hartmut Kaiser
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/phylanx.hpp>
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/lcos.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+#include <blaze/Math.h>
+
+phylanx::execution_tree::primitive_argument_type compile_and_run(
+    std::string const& codestr)
+{
+    phylanx::execution_tree::compiler::function_list snippets;
+    phylanx::execution_tree::compiler::environment env =
+        phylanx::execution_tree::compiler::default_environment();
+
+    auto const& code = phylanx::execution_tree::compile(codestr, snippets, env);
+    return code.run();
+}
+
+void test_clip_PhySL_0()
+{
+    std::string const code = R"(block(
+        define(a, 3),
+        define(b, [0, 4, -1]),
+        define(c, [[5, 0, 2], [0, 0, 1]]),
+        clip(a, b, c))
+    )";
+
+    auto result = phylanx::execution_tree::extract_integer_value_strict(
+        compile_and_run(code));
+
+    HPX_TEST_EQ(result,
+        phylanx::ir::node_data<std::int64_t>(
+            blaze::DynamicMatrix<std::int64_t>{{3, 4, 2}, {0, 4, 1}}));
+}
+
+void test_clip_PhySL_1()
+{
+    std::string const code = R"(block(
+        define(a, [6.0, 1.0, 9.0]),
+        define(b, [0.0, 4.0, 2.0]),
+        clip(a, b, nil))
+    )";
+
+    auto result =
+        phylanx::execution_tree::extract_numeric_value(compile_and_run(code));
+
+    HPX_TEST_EQ(result,
+        phylanx::ir::node_data<double>(
+            blaze::DynamicVector<double>{6.0, 4.0, 9.0}));
+}
+
+void test_clip_PhySL_2()
+{
+    std::string const code = R"(block(
+        define(a, [[1.0, 2.0, 3.0],[6.0, 1.0, 9.0]]),
+        define(b, [0.0, 4.0, 2.0]),
+        clip(a, nil, b))
+    )";
+
+    auto result =
+        phylanx::execution_tree::extract_numeric_value(compile_and_run(code));
+
+    HPX_TEST_EQ(result,
+        phylanx::ir::node_data<double>(
+            blaze::DynamicMatrix<double>{{0.0, 2.0, 2.0}, {0.0, 1.0, 2.0}}));
+}
+
+void test_clip_0d()
+{
+    phylanx::execution_tree::primitive arg =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(10.0));
+
+    phylanx::execution_tree::primitive min =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(12.0));
+
+    phylanx::execution_tree::primitive max =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(14.0));
+
+    phylanx::execution_tree::primitive p =
+        phylanx::execution_tree::primitives::create_clip(hpx::find_here(),
+            phylanx::execution_tree::primitive_arguments_type{
+                std::move(arg), std::move(min), std::move(max)});
+
+    hpx::future<phylanx::execution_tree::primitive_argument_type> f = p.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(12.0),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+void test_clip_1d()
+{
+    blaze::DynamicVector<double> v1{-1.0, 0.0, 1.0, 2.0, 4.0, 5.0};
+    blaze::DynamicVector<double> v2{3.0, 1.0, -1.0, 2.0, 3.0, 4.0};
+    blaze::DynamicVector<double> expected{3.0, 1.0, 1.0, 2.0, 4.0, 5.0};
+
+    phylanx::execution_tree::primitive arg =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(v1));
+
+    phylanx::execution_tree::primitive min =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(v2));
+
+    phylanx::execution_tree::primitive max =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(9.0));
+
+    phylanx::execution_tree::primitive p =
+        phylanx::execution_tree::primitives::create_clip(hpx::find_here(),
+            phylanx::execution_tree::primitive_arguments_type{
+                std::move(arg), std::move(min), std::move(max)});
+
+    hpx::future<phylanx::execution_tree::primitive_argument_type> f = p.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(expected),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+void test_clip_2d()
+{
+    blaze::DynamicMatrix<double> v1{
+        {-1.0, 0.0, 1.0, 2.0, 4.0, 5.0}, {3.0, 1.0, -1.0, 2.0, 3.0, 8.0}};
+    blaze::DynamicVector<double> v2{2.0, -1.0, 3.0, 2.0, 5.0, 8.0};
+    blaze::DynamicMatrix<double> expected{
+        {1.5, 1.5, 1.5, 2.0, 4.0, 5.0}, {2.0, 1.5, 1.5, 2.0, 3.0, 8.0}};
+
+    phylanx::execution_tree::primitive arg =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(v1));
+
+    phylanx::execution_tree::primitive min =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(1.5));
+
+    phylanx::execution_tree::primitive max =
+        phylanx::execution_tree::primitives::create_variable(
+            hpx::find_here(), phylanx::ir::node_data<double>(v2));
+
+    phylanx::execution_tree::primitive p =
+        phylanx::execution_tree::primitives::create_clip(hpx::find_here(),
+            phylanx::execution_tree::primitive_arguments_type{
+                std::move(arg), std::move(min), std::move(max)});
+
+    hpx::future<phylanx::execution_tree::primitive_argument_type> f = p.eval();
+
+    HPX_TEST_EQ(phylanx::ir::node_data<double>(expected),
+        phylanx::execution_tree::extract_numeric_value(f.get()));
+}
+
+int main(int argc, char* argv[])
+{
+    test_clip_0d();
+    test_clip_1d();
+    test_clip_2d();
+    test_clip_PhySL_0();
+    test_clip_PhySL_1();
+    test_clip_PhySL_2();
+
+    return hpx::util::report_errors();
+}
