0e2f146feb4d72358912c5e90865ed37b368cb3d
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-01-16 07:44:41 -0600

    Add numpy compatible naming scheme for logical operations
    - added logical_xor primitive

--- phylanx/plugins/booleans/xor_operation.hpp
+++ phylanx/plugins/booleans/xor_operation.hpp
@@ -1,10 +1,10 @@
-//  Copyright (c) 2017-2019 Hartmut Kaiser
+//  Copyright (c) 2019 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(PHYLANX_PRIMITIVES_OR_OPERATION_OCT_06_2017_0522PM)
-#define PHYLANX_PRIMITIVES_OR_OPERATION_OCT_06_2017_0522PM
+#if !defined(PHYLANX_PRIMITIVES_XOR_OPERATION_JAN_15_2019_0417PM)
+#define PHYLANX_PRIMITIVES_XOR_OPERATION_JAN_15_2019_0417PM
 
 #include <phylanx/config.hpp>
 #include <phylanx/plugins/booleans/logical_operation.hpp>
@@ -18,29 +18,29 @@ namespace phylanx { namespace execution_tree { namespace primitives
     ///////////////////////////////////////////////////////////////////////////
     namespace detail
     {
-        struct or_op;
+        struct xor_op;
     }
 
     ///////////////////////////////////////////////////////////////////////////
-    class or_operation : public logical_operation<detail::or_op>
+    class xor_operation : public logical_operation<detail::xor_op>
     {
-        using base_type = logical_operation<detail::or_op>;
+        using base_type = logical_operation<detail::xor_op>;
 
     public:
         static match_pattern_type const match_data[2];
 
-        or_operation() = default;
+        xor_operation() = default;
 
-        or_operation(primitive_arguments_type&& operands,
+        xor_operation(primitive_arguments_type&& operands,
             std::string const& name, std::string const& codename);
     };
 
-    inline primitive create_or_operation(hpx::id_type const& locality,
+    inline primitive create_xor_operation(hpx::id_type const& locality,
         primitive_arguments_type&& operands,
         std::string const& name = "", std::string const& codename = "")
     {
         return create_primitive_component(
-            locality, "__or", std::move(operands), name, codename);
+            locality, "__xor", std::move(operands), name, codename);
     }
 }}}
 
