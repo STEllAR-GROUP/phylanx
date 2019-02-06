1614196d7285dbcbb27d757414b6b4475db6653f
monil01 <monil01@gmail.com>
2019-02-06 10:33:17 -0800

    primitive is registered after some calls and not from the constructor

--- phylanx/execution_tree/primitives/primitive_component_base.hpp
+++ phylanx/execution_tree/primitives/primitive_component_base.hpp
@@ -142,6 +142,9 @@ namespace phylanx { namespace execution_tree
             std::int64_t get_exec_hysteresis() const;
             std::int64_t exec_threshold_ = 425000;
             std::int64_t exec_hysteresis_ = 75000;
+		
+            hpx::launch select_direct_eval_execution_policy(hpx::launch policy);
+	    bool policy_initialized_ = false;
 
 	    std::unique_ptr<phylanx::util::apex_direct_vs_nondirect_policy> 
 		direct_vs_nondirect_policy_instance;
