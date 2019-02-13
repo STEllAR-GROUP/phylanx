5f2b9ea5ca4b5ac0a5aa568704f6d1b0cfdeed48
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-02-13 17:19:10 -0600

    Splitting TUs to reduce compiler memory requirements

---
+++ src/plugins/matrixops/dot_operation_int64.cpp
@@ -0,0 +1,67 @@
+//  Copyright (c) 2017-2018 Hartmut Kaiser
+//  Copyright (c) 2017 Parsa Amini
+//  Copyright (c) 2019 Bita Hasheminezhad
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/config.hpp>
+#include <phylanx/ir/node_data.hpp>
+#include <phylanx/plugins/matrixops/dot_operation.hpp>
+#include <phylanx/plugins/matrixops/dot_operation_impl.hpp>
+
+#include <cstdint>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+    // explicitly instantiate the required functions
+
+    ///////////////////////////////////////////////////////////////////////////
+    template blaze::DynamicVector<std::int64_t> dot_operation::convert_to_1d(
+        ir::node_data<std::int64_t>&&) const;
+
+    ///////////////////////////////////////////////////////////////////////////
+    template primitive_argument_type dot_operation::dot0d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+    template primitive_argument_type dot_operation::dot1d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+    template primitive_argument_type dot_operation::dot2d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+#if defined(PHYLANX_HAVE_BLAZE_TENSOR)
+    template primitive_argument_type dot_operation::dot3d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+#endif
+
+    ///////////////////////////////////////////////////////////////////////////
+    template primitive_argument_type dot_operation::outer_nd_helper(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+    template primitive_argument_type dot_operation::outer1d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+    template primitive_argument_type dot_operation::outer2d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+#if defined(PHYLANX_HAVE_BLAZE_TENSOR)
+    template primitive_argument_type dot_operation::outer3d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+#endif
+
+    ///////////////////////////////////////////////////////////////////////////
+    template primitive_argument_type dot_operation::contraction2d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+
+#if defined(PHYLANX_HAVE_BLAZE_TENSOR)
+    template primitive_argument_type dot_operation::contraction3d(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&) const;
+#endif
+
+    ///////////////////////////////////////////////////////////////////////////
+    template primitive_argument_type dot_operation::tensordot_range_of_scalars(
+        ir::node_data<std::int64_t>&&, ir::node_data<std::int64_t>&&, val_type,
+        val_type) const;
+}}}
