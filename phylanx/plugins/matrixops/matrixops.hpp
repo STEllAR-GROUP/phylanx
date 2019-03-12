5503a1197efe686ecd736e4ac872f11493a38721
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-03-12 14:56:36 -0500

    Adding size primitive

--- phylanx/plugins/matrixops/matrixops.hpp
+++ phylanx/plugins/matrixops/matrixops.hpp
@@ -35,6 +35,7 @@
 #include <phylanx/plugins/matrixops/repeat_operation.hpp>
 #include <phylanx/plugins/matrixops/reshape_operation.hpp>
 #include <phylanx/plugins/matrixops/shuffle_operation.hpp>
+#include <phylanx/plugins/matrixops/size.hpp>
 #include <phylanx/plugins/matrixops/slicing_operation.hpp>
 #include <phylanx/plugins/matrixops/softmax_operation.hpp>
 #include <phylanx/plugins/matrixops/squeeze_operation.hpp>
---
+++ phylanx/plugins/matrixops/size.hpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2017-2019 Hartmut Kaiser
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(PHYLANX_PRIMITIVES_SIZE_MAR_12_2019_1254PM)
+#define PHYLANX_PRIMITIVES_SIZE_MAR_12_2019_1254PM
+
+#include <phylanx/config.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+#include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+
+#include <hpx/lcos/future.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+    class size_operation
+      : public primitive_component_base
+      , public std::enable_shared_from_this<size_operation>
+    {
+    protected:
+        hpx::future<primitive_argument_type> eval(
+            primitive_arguments_type const& operands,
+            primitive_arguments_type const& args,
+            eval_context ctx) const override;
+
+    public:
+        static match_pattern_type const match_data;
+
+        size_operation() = default;
+
+        size_operation(primitive_arguments_type&& params,
+            std::string const& name, std::string const& codename);
+    };
+
+    inline primitive create_size_operation(hpx::id_type const& locality,
+        primitive_arguments_type&& operands,
+        std::string const& name = "", std::string const& codename = "")
+    {
+        return create_primitive_component(
+            locality, "size", std::move(operands), name, codename);
+    }
+}}}
+
+#endif
+
+
