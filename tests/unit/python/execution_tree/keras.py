e9351536fd37c4ab295f881eefc544edd4bb1b38
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-03-21 21:43:06 +0000

    Add a test

---
+++ tests/unit/python/execution_tree/keras.py
@@ -0,0 +1,42 @@
+#  Copyright (c) 2019-2020 Hartmut Kaiser
+#  Copyright (c) 2019 Steven R. Brandt
+#
+#  Distributed under the Boost Software License, Version 1.0. (See accompanying
+#  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+from phylanx import Phylanx
+import numpy as np
+
+
+def softmax(x, axis=-1):
+    y = np.exp(x - np.max(x, axis, keepdims=True))
+    return y / np.sum(y, axis, keepdims=True)
+
+
+def categorical_crossentropy(target, output, from_logits=False):
+    if from_logits:
+        output = softmax(output)
+    else:
+        output /= output.sum(axis=-1, keepdims=True)
+    output = np.clip(output, 1e-7, 1 - 1e-7)
+    rep = target * -np.log(output)
+    return np.sum(rep, axis=-1, keepdims=False)
+
+
+@Phylanx
+def cc(target, output, from_logits):
+    print("before:",output)
+    res = categorical_crossentropy(target, output, from_logits)
+    print("after:",output)
+    return res
+
+
+# Generate some random data
+t = np.linspace(1, 10, 10)
+t[2] = 10
+o = 3*np.ones(10)
+o[4] = 2
+
+flag = False
+v1 = cc(t, o, flag)
+v2 = categorical_crossentropy(t, o, flag)
+assert v1 == v2
