481dd42cabb9164dea4ec116a3a93a90e75eff51
R. Tohid <tohid.rod@gmail.com>
2019-04-17 14:08:17 -0500

    Fix licenses.

--- src/plugins/matrixops/matrixops.cpp
+++ src/plugins/matrixops/matrixops.cpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2018 Hartmut Kaiser
+//  Copyright (c) 2019 R. Tohid
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
