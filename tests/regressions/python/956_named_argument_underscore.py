6a8913396097846ae6e5bf6149c02f2f3ea9d404
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2019-04-23 15:22:31 -0500

    Fixing flake issues

--- tests/regressions/python/956_named_argument_underscore.py
+++ tests/regressions/python/956_named_argument_underscore.py
@@ -10,13 +10,18 @@ import numpy as np
 from phylanx import Phylanx
 
 
+# silence flake
+def ctc_decode(y_pred, input_length, greedy, beam_width):
+    pass
+
+
 @Phylanx
 def ctc_decode_eager(y_pred, input_length, greedy=True, beam_width=100):
     return ctc_decode(y_pred, input_length, greedy=greedy, beam_width=beam_width)
 
 
-y_pred = np.array([[[1,2,3,4,5]]])
-input_length = np.array([1,5])
+y_pred = np.array([[[1, 2, 3, 4, 5]]])
+input_length = np.array([1, 5])
 
 r = ctc_decode_eager(y_pred, input_length, True, 100)
 assert np.allclose(r, [np.array([[4.]]), np.array([[-1.60943791]])]), r
