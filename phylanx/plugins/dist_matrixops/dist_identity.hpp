35c426b7337940a6167f417602c1a5728571f451
nanmiao <nanmiaowu@gmail.com>
2020-04-01 14:51:00 -0500

    add draft test

---
+++ phylanx/plugins/dist_matrixops/dist_identity.hpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2020 Nanmiao Wu
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(PHYLANX_PRIMITIVES_DIST_RANDOM)
+#define PHYLANX_PRIMITIVES_DIST_RANDOM
+
+#include <phylanx/config.hpp>
+#include <phylanx/execution_tree/primitives/base_primitive.hpp>
+#include <phylanx/execution_tree/primitives/node_data_helpers.hpp>
+#include <phylanx/execution_tree/primitives/primitive_component_base.hpp>
+#include <phylanx/util/random.hpp>
+
+#include <hpx/lcos/future.hpp>
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <random>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <blaze/Math.h>
+
+namespace phylanx { namespace dist_matrixops { namespace primitives
+{
+    class dist_identity
+      : public execution_tree::primitives::primitive_component_base
+      , public std::enable_shared_from_this<dist_identity>
+    {
+    public:
+        static execution_tree::match_pattern_type const match_data;
+
+        dist_identity() = default;
+
+        dist_identity(execution_tree::primitive_arguments_type&& operands,
+            std::string const& name, std::string const& codename);
+
+    protected:
+        hpx::future<execution_tree::primitive_argument_type> eval(
+            execution_tree::primitive_arguments_type const& operands,
+            execution_tree::primitive_arguments_type const& args,
+            execution_tree::eval_context ctx) const override;
+
+        execution_tree::primitive_argument_type dist_identity_helper(
+            std::int64_t&& sz, std::uint32_t const& tile_idx, 
+            std::uint32_t const& numtiles, std::string&& given_name, 
+            std::string const& tiling_type) const;   
+
+        execution_tree::primitive_argument_type dist_identity(
+            std::int64_t&& sz, std::uint32_t const& tile_idx, 
+            std::uint32_t const& numtiles, std::string&& given_name, 
+            std::string const& tiling_type, 
+            execution_tree::node_data_type dtype) const;
+
+        
+    };
+
+    inline execution_tree::primitive create_dist_identity(hpx::id_type const& locality,
+        execution_tree::primitive_arguments_type&& operands,
+        std::string const& name = "", std::string const& codename = "")
+    {
+        return create_primitive_component(
+            locality, "identity_d", std::move(operands), name, codename);
+    }
+}}}
+
+#endif
