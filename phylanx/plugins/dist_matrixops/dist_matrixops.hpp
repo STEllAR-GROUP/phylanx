139772dcdc18ec4559b186219a39b61d13e4f2ad
nanmiao <nanmiaowu@gmail.com>
2020-04-02 16:24:39 -0500

    change order of i

--- phylanx/plugins/dist_matrixops/dist_matrixops.hpp
+++ phylanx/plugins/dist_matrixops/dist_matrixops.hpp
@@ -9,8 +9,9 @@
 #include <phylanx/plugins/dist_matrixops/dist_cannon_product.hpp>
 #include <phylanx/plugins/dist_matrixops/dist_constant.hpp>
 #include <phylanx/plugins/dist_matrixops/dist_dot_operation.hpp>
+#include <phylanx/plugins/dist_matrixops/dist_identity.hpp>
 #include <phylanx/plugins/dist_matrixops/dist_random.hpp>
 #include <phylanx/plugins/dist_matrixops/dist_transpose_operation.hpp>
-#include <phylanx/plugins/dist_matrixops/dist_identity.hpp>
+
 
 #endif
