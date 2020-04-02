355b30d6b667b6e5c369b18eabcb4023a72701aa
nanmiao <nanmiaowu@gmail.com>
2020-04-02 16:22:26 -0500

    change the order

--- src/plugins/dist_matrixops/dist_identity.cpp
+++ src/plugins/dist_matrixops/dist_identity.cpp
@@ -1,4 +1,6 @@
 // Copyright (c) 2020 Nanmiao Wu
+// Copyright (c) 2020 Bita Hasheminezhad
+// Copyright (c) 2017-2020 Hartmut Kaiser
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
