08f6ed838b7446f7bff77802ac54cf05f60bfaaf
nanmiao <nanmiaowu@gmail.com>
2020-04-03 15:37:32 -0500

    5x5 matrix, column

--- tests/unit/plugins/dist_matrixops/dist_identity_2_loc.cpp
+++ tests/unit/plugins/dist_matrixops/dist_identity_2_loc.cpp
@@ -109,6 +109,36 @@ void test_identity_2loc_1()
     }
 }
 
+void test_identity_2loc_2()
+{
+    if (hpx::get_locality_id() == 0)
+    {
+        test_identity_d_operation("test_identity_2loc_2", R"(
+            identity_d(5, 0, 2, "my_identity_3", "column")
+        )", R"(
+            annotate_d([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0], 
+                [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]], 
+                "my_identity_3",
+                list("args",
+                    list("locality", 0, 2),
+                    list("tile", list("columns", 0, 3), list("rows", 0, 5))))
+        )");
+    }
+    else
+    {
+        test_identity_d_operation("test_identity_2loc_2", R"(
+            identity_d(5, 1, 2, "my_identity_3", "column")
+        )", R"(
+            annotate_d([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [1.0, 0.0], 
+                [0.0, 1.0]], 
+                "my_identity_3",
+                list("args",
+                    list("locality", 1, 2),
+                    list("tile", list("columns", 3, 5), list("rows", 0, 5))))
+        )");
+    }
+}
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -117,6 +147,7 @@ int hpx_main(int argc, char* argv[])
     // only annotations are compared
     test_identity_2loc_0();
     test_identity_2loc_1();
+    test_identity_2loc_2();
     
 
     hpx::finalize();
