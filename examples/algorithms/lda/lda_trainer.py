332dee30552cc0beddb765f800c676ae6bc61252
ct.clmsn <ct.clmsn@gmail.com>
2020-05-03 21:34:18 -0400

    fixed issues

--- examples/algorithms/lda/lda_trainer.py
+++ examples/algorithms/lda/lda_trainer.py
@@ -3,34 +3,52 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
+# Algorithm from the following references
+#
+# D. Newman, A. Asuncion, P. Smyth, M. Welling.
+#   "Distributed Algorithms for Topic Models." JMLR 2009.
+# D. Newman, A. Asuncion, P. Smyth, M. Welling.
+#   "Distributed Inference for Latent Dirichlet Allocation." NIPS 2007.
+#
 
-from random import random as rnd, randint
-from numpy import *
+
+from random import randint, random as rnd
+from numpy import zeros, ones, sum
+
 
 # https://stackoverflow.com/questions/7287014/is-there-any-drand48-equivalent-in-python-or-a-wrapper-to-it/7287046#7287046
 #
+'''
 class Rand48(object):
     def __init__(self, seed):
         self.n = seed
+
     def seed(self, seed):
         self.n = seed
+
     def srand(self, seed):
         self.n = (seed << 16) + 0x330e
+
     def next(self):
         self.n = (25214903917 * self.n + 11) & (2**48 - 1)
         return self.n
+
     def drand(self):
         return self.next() / 2**48
+
     def lrand(self):
         return self.next() >> 17
+
     def mrand(self):
         n = self.next() >> 16
         if n & (1 << 31):
             n -= 1 << 32
-        return n   
+        return n
+'''
+
 
 def gibbs(wdc, a, b, z, wp, dp, ztot):
-    drand = Rand48(0)
+    # drand = Rand48(0)
     W = wdc.shape[1]
     D = wdc.shape[0]
     T = ztot.shape[0]
@@ -47,7 +65,7 @@ def gibbs(wdc, a, b, z, wp, dp, ztot):
     for d in range(D):
         for w in range(W):
 
-            wdf = wdc[d,w]
+            wdf = wdc[d, w]
 
             # skip over zero entries
             #
@@ -56,14 +74,14 @@ def gibbs(wdc, a, b, z, wp, dp, ztot):
 
             for f in range(int(wdf)):
                 t = int(z[n])
-                ztot[t]-=1.0
-                wp[w, t]-=1.0
-                dp[d, t]-=1.0
+                ztot[t] -= 1.0
+                wp[w, t] -= 1.0
+                dp[d, t] -= 1.0
 
-                lhs[:] = wp[w,:] + b
-                rhs[:] = dp[d,:] + a
+                lhs[:] = wp[w, :] + b
+                rhs[:] = dp[d, :] + a
                 zprob[:] = ztot[:] + wbeta
-                probs[:] = lhs * ( rhs / zprob )
+                probs[:] = lhs * (rhs / zprob)
 
                 # algorithmic flaw, tends to oversample
                 # from a single topic
@@ -93,27 +111,28 @@ def gibbs(wdc, a, b, z, wp, dp, ztot):
 
                 # inspired by: https://www.youtube.com/watch?v=aHLslaWO-AQ
                 #
-                maxprob = abs(drand.drand()) * max(probs) * 2.0
-                t = int(abs(drand.drand()) * float(T))
+                # maxprob = abs(drand.drand()) * max(probs) * 2.0
+                # t = int(abs(drand.drand()) * float(T))
+                maxprob = abs(rnd()) * max(probs) * 2.0
+                t = int(abs(rnd()) * float(T))
                 while maxprob > 1e-10:
                     maxprob -= probs[t]
                     t = ((t+1) % T)
 
-                z[n] = t;
-                ztot[t]+=1.0
-                wp[w, t]+=1.0
-                dp[d, t]+=1.0
-                n+=1
+                z[n] = t
+                ztot[t] += 1.0
+                wp[w, t] += 1.0
+                dp[d, t] += 1.0
+                n += 1
 
-    assert N == n, "N != n, (%d, %d)" % (N, n)
     return (z, ztot, wp, dp)
+
 
 def lda_trainer(wdc, T, a=0.1, b=0.01, iters=500):
     alpha, beta = a, b
     D, W = wdc.shape[0], wdc.shape[1]
     N = int(sum(wdc))
 
-    wbeta = float(W) * beta
     z = zeros(N)
     wp = zeros((W, T))
     dp = zeros((D, T))
@@ -123,43 +142,38 @@ def lda_trainer(wdc, T, a=0.1, b=0.01, iters=500):
 
     for n in range(N):
         z[n] = randint(0, T-1)
-    
+
     k = 0
     for i in range(W):
         for j in range(int(wfreq[i])):
-            wp[i, int(z[k])]+=1.0
-            k+=1
-    
+            wp[i, int(z[k])] += 1.0
+            k += 1
+
     k = 0
     for i in range(D):
         for j in range(int(dfreq[i])):
-            dp[i, int(z[k])]+=1.0
-            k+=1
+            dp[i, int(z[k])] += 1.0
+            k += 1
 
     ztot0 = zeros(T)
     wp0 = zeros(wp.shape)
 
     for i in range(iters):
-        wp0[:,:] = wp[:,:]
-        ztot0[:] = sum(wp,0)
-        #print(z[4116])
-        #print('pg', wp[0, :])
+        wp0[:, :] = wp[:, :]
+        ztot0[:] = sum(wp, 0)
         res = gibbs(wdc, alpha, beta, z, wp, dp, ztot0)
         z[:] = res[0]
         ztot0[:] = res[1]
         wp[:] = res[2]
-        #print('ag', wp[0, :])
         dp[:] = res[3]
-        #print('*')
-        #print(z[4116])
-        #print()
-        wp[:,:] = wp0 + (wp - wp0)
-        #print(i)
+        wp[:, :] = wp0 + (wp - wp0)
+        # print loop counter
+        # print(i)
 
-    print(wp, dp, z)
+    return (wp, dp, z)
+
 
 if __name__ == "__main__":
-    wdc = ones((1000,100))
+    wdc = ones((1000, 100))
     res = lda_trainer(wdc, 3, iters=20)
     print(res)
-
