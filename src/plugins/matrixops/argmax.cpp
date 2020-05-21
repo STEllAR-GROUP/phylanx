e60cb8465076992f20d06c2dbf3c269f4f52dce0
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2020-05-21 16:51:38 -0500

    Refactoring argminmax into common module

--- src/plugins/matrixops/argmax.cpp
+++ src/plugins/matrixops/argmax.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 Parsa Amini
-// Copyright (c) 2018-2019 Hartmut Kaiser
+// Copyright (c) 2018-2020 Hartmut Kaiser
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -35,31 +35,6 @@ namespace phylanx { namespace execution_tree { namespace primitives
             The index of the maximum value in the array. If an axis is
             specified, a vector of maxima along the axis is returned.)")
     };
-
-    ///////////////////////////////////////////////////////////////////////////
-    namespace detail
-    {
-        struct argmax_op
-        {
-            template <typename T>
-            static T initial()
-            {
-                return util::detail::numeric_limits_min<T>();
-            }
-
-            template <typename T>
-            static bool compare(T lhs, T rhs)
-            {
-                return lhs > rhs;
-            }
-
-            template <typename Iter>
-            Iter operator()(Iter begin, Iter end) const
-            {
-                return std::max_element(begin, end);
-            }
-        };
-    }
 
     ///////////////////////////////////////////////////////////////////////////
     argmax::argmax(primitive_arguments_type&& operands,
