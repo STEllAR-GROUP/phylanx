6ade5f0f02ed1cc41b6211619a96844af571d4bc
nanmiao <nanmiaowu@gmail.com>
2020-06-12 16:53:07 -0500

    2 loc pass; multi-examples

--- tests/unit/plugins/dist_matrixops/all_gather_2_loc.cpp
+++ tests/unit/plugins/dist_matrixops/all_gather_2_loc.cpp
@@ -38,7 +38,6 @@ void test_all_gather_d_operation(std::string const& name,
         compile_and_run(name, expected_str);
 
     HPX_TEST_EQ(hpx::cout, result, comparison);
-    std::cout << result;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -56,7 +55,7 @@ void test_all_gather_2d_0()
     }
     else
     {
-        test_all_gather_d_operation("test1d_0", R"(
+        test_all_gather_d_operation("test2d_0", R"(
             all_gather_d(
                 annotate_d([[7, 8, 9]], "array_0",
                     list("tile", list("columns", 0, 3), list("rows", 2, 3))
@@ -66,15 +65,61 @@ void test_all_gather_2d_0()
     }
 }
 
+void test_all_gather_2d_1()
+{
+    if (hpx::get_locality_id() == 0)
+    {
+        test_all_gather_d_operation("test2d_1", R"(
+            all_gather_d(
+                annotate_d([[1, 4], [2, 5], [3, 6]], "array_1",
+                    list("tile", list("columns", 0, 2), list("rows", 0, 3))
+                )
+            )
+        )", "[[1, 4, 7], [2, 5, 8], [3, 6, 9]]");
+    }
+    else
+    {
+        test_all_gather_d_operation("test2d_1", R"(
+            all_gather_d(
+                annotate_d([[7], [8], [9]], "array_1",
+                    list("tile", list("columns", 2, 3), list("rows", 0, 3))
+                )
+            )
+        )", "[[1, 4, 7], [2, 5, 8], [3, 6, 9]]");
+    }
+}
 
-
-
+void test_all_gather_2d_2()
+{
+    if (hpx::get_locality_id() == 0)
+    {
+        test_all_gather_d_operation("test2d_2", R"(
+            all_gather_d(
+                annotate_d([[1, 2, 3, 4], [5, 6, 7, 8]], "array_2",
+                    list("tile", list("columns", 0, 4), list("rows", 0, 2))
+                )
+            )
+        )", "[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]");
+    }
+    else
+    {
+        test_all_gather_d_operation("test2d_2", R"(
+            all_gather_d(
+                annotate_d([[9, 10, 11, 12], [13, 14, 15, 16]], "array_2",
+                    list("tile", list("columns", 0, 4), list("rows", 2, 4))
+                )
+            )
+        )", "[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]");
+    }
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
 int hpx_main(int argc, char* argv[])
 {
     test_all_gather_2d_0();
+    test_all_gather_2d_1();
+    test_all_gather_2d_2();
 
     hpx::finalize();
     return hpx::util::report_errors();
