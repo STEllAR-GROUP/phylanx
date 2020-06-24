f334a4b09cdc4a749b314028658705ccbd46629a
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-06-24 20:38:51 +0000

    Add missing file

---
+++ tests/unit/python/execution_tree/modulus_test.py
@@ -0,0 +1,21 @@
+from phylanx import Phylanx
+import numpy as np
+
+
+def mod_test():
+    v = np.array([5, 8, 3, 7])
+    v %= 2
+    q = np.array([5, 8, 3, 7])
+    q = 2 % q
+    return [v, q]
+
+
+mod_test2 = Phylanx(mod_test)
+r1 = mod_test()
+r2 = mod_test2()
+for i in range(len(r1)):
+    if not np.all(r1[i] == r2[i]):
+        print("Differences found:",i)
+        print(r1[i])
+        print(r2[i])
+        assert False
