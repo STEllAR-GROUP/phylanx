9e35e52b422014cd2eb43debf69ccc0152a84441
git-kale <kalemaheshj@gmail.com>
2020-08-26 16:30:56 +0530

    Correct the misspelled import statement in ir tests

--- tests/unit/python/ir/test_symbol_table.py
+++ tests/unit/python/ir/test_symbol_table.py
@@ -4,7 +4,7 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 from collections import defaultdict
-import rhylanx.ir as ir
+import phylanx.ir as ir
 
 
 class TestNameSpace:
